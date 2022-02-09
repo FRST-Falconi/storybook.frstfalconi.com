8f8174dc15a14789da64f6e4c0526a6f6b69e0fe
Caio Godoy <caioafgod@gmail.com>
2022-02-09 20:27:05 -0300

    feat - fixing dependencies

---
+++ src/cards.user.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import UserCard from './cards/user/index'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Cards/User',
+    component: UserCard,
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <UserCard {...args} />
+
+export const Loading = Template.bind({})
+Loading.args = {
+    loading: true,
+}
---
+++ src/cards/user/index.tsx
@@ -0,0 +1,28 @@
+import '../../shared/global.css'
+import { useState, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+
+interface UserCardProps {
+    loading: boolean
+}
+
+
+/**
+ * @param {UserCardProps} props
+ */
+export default function CalendarCard(props: UserCardProps) {
+    const { t } = useTranslation()
+
+    const [selected, setSelected] = useState(false)
+
+    return (
+        <div style={{ height: '100%' }}>
+            {
+                props.loading ?
+                    <div className='shimmer' style={{ minHeight: '100px' }}></div>
+                    :
+                    <div className='cardContent'>NotLoading</div>
+            }
+        </div >
+    )
+}
