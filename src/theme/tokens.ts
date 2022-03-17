2d326058bf1cf6b3307d537ec5fe843f3f37e1ad
Lucas Santos <fclucasnet@gmail.com>
2022-03-17 17:58:31 -0300

    feature: Add theme to manage design tokens

---
+++ src/theme/tokens.ts
@@ -0,0 +1,42 @@
+export const DesignTokens = {
+    colors: { 
+        primary: { 
+            primary1: '#FF4D0D',
+            primary2: '#EE4C15',
+            primary3: '#D14211',
+            primary4: '#6A3F86',
+        },
+
+        message: {
+            success1: '#2CA92A',
+            success2: '#D1F6D1',
+            alert1: '#FFC200',
+            alert2: '#FEF0D4',
+            error1: '#FF0000',
+            error2: '#FFE5E5',
+        },
+
+        neutral: {
+            grey1: '#222222',
+            grey2: '#757575',
+            grey3: '#9C9C9C',
+            grey4: '#BDBDBD',
+            grey5: '#E0E0E0',
+            grey6: '#EBEBEB',
+            grey7: '#F5F5F5',
+            grey8: '#FAFAFA',
+        },
+
+        shades: { 
+            white: '#FFFFFF',
+            black: '#000000',
+        }
+    },
+
+    text: {
+        primary: '#000000',
+        secondary: '#222222',
+        tertiary: '#757575',
+        card: '#FF4D0D'
+    }
+}
