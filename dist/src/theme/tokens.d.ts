2202d1d1b073787ca3a4030b005a46a10ad1a1dd
Lucas Santos <fclucasnet@gmail.com>
2022-04-13 15:38:28 -0300

    v0.2.9 (#4)

---
+++ dist/src/theme/tokens.d.ts
@@ -0,0 +1,190 @@
+export declare const DesignTokens: {
+    colors: {
+        borderPrimary: string;
+        borderSecondary1: string;
+        borderSecondary2: string;
+        borderSecondary3: string;
+        borderSecondary4: string;
+        borderSecondary5: string;
+        linkDefaultOnfocus: string;
+        linkDisabled: string;
+        linkHover: string;
+        linkOnfocus: string;
+        linkPressed: string;
+        messageAlert1: string;
+        messageAlert2: string;
+        messageError1: string;
+        messageError2: string;
+        messageSuccess1: string;
+        messageSuccess2: string;
+        neutralsGrey1: string;
+        neutralsGrey2: string;
+        neutralsGrey3: string;
+        neutralsGrey4: string;
+        neutralsGrey5: string;
+        neutralsGrey6: string;
+        neutralsGrey7: string;
+        neutralsGrey8: string;
+        primary1: string;
+        primary2: string;
+        primary3: string;
+        secondary1: string;
+        secondary2: string;
+        secondary5: string;
+        shadeWhite: string;
+        shadeBlack: string;
+    };
+    fonts: {
+        header1: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        header2: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsBodyBold: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsBodyRegular: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsBodySmall: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsBodyTiny: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsCardTitle: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+        textMessageComponentsHero: {
+            fontFamily: string;
+            fontSize: string;
+            letterSpacing: string;
+            lineHeight: string;
+            fontWeight: number;
+            color: string;
+        };
+    };
+    global: {
+        shadowLevel_0: {
+            value: {
+                x: string;
+                y: string;
+                blur: string;
+                spread: string;
+                color: string;
+                type: string;
+            };
+            type: string;
+        };
+        shadowLevel_1: {
+            value: {
+                x: string;
+                y: string;
+                blur: string;
+                spread: string;
+                color: string;
+                type: string;
+            };
+            type: string;
+        };
+        shadowLevel_2: {
+            value: {
+                x: string;
+                y: string;
+                blur: string;
+                spread: string;
+                color: string;
+                type: string;
+            };
+            type: string;
+        };
+        shadowLevel_3: {
+            value: {
+                x: string;
+                y: string;
+                blur: string;
+                spread: string;
+                color: string;
+                type: string;
+            };
+            type: string;
+        };
+        border_width_small: {
+            value: string;
+            type: string;
+        };
+        border_width_medium: {
+            value: string;
+            type: string;
+        };
+        "border-width-large": {
+            value: string;
+            type: string;
+        };
+        "border-radius-small": {
+            value: string;
+            type: string;
+        };
+        "border-radius-medium": {
+            value: string;
+            type: string;
+        };
+        "border-radius-large": {
+            value: string;
+            type: string;
+        };
+        "border-radius-xlarge": {
+            value: string;
+            type: string;
+        };
+        button_padding: {
+            value: string;
+            type: string;
+        };
+        card_padding_normal: {
+            value: string;
+            type: string;
+        };
+        card_padding_large: {
+            value: string;
+            type: string;
+        };
+    };
+};
+//# sourceMappingURL=tokens.d.ts.map
