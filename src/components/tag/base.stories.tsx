d45d92fb2c19e2d74807ed80301e8a555f5610b8
Caio Godoy <caioafgod@gmail.com>
2022-04-13 19:23:49 +0000

    adding new variant to documentation and changing version number

--- src/components/tag/base.stories.tsx
+++ src/components/tag/base.stories.tsx
@@ -11,6 +11,15 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Tag {...args} />
 
+export const loading = Template.bind({})
+loading.args = {
+    loading: true,
+    inverted: false,
+    selected: false,
+    color: "#000",
+    title: 'Titulo Tag',
+}
+
 export const normal = Template.bind({})
 normal.args = {
     loading: false,
