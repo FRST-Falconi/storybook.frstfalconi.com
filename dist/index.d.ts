f195cb9a9efc42e9668bbc2e4d505698f0474415
Lucas Santos <fclucasnet@gmail.com>
2022-04-18 18:21:47 -0300

    v0.2.11

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,5 +4,5 @@ export { default as UserCard } from "./src/components/cards/user";
 export { default as CalendarCard } from "./src/components/calendar";
 export { default as Button } from "./src/components/buttons";
 export { default as TextField } from './src/components/form-elements/textfield';
-export { AlertCicle as AlertCicleIcon } from './src/shared/icons';
+export { default as AlertCicle } from './src/shared/icons';
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -101,6 +101,7 @@ function CheckboxEmpty() {
 function AlertCicle({ fill }) {
     return (jsxRuntime.jsxs("svg", { width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: [jsxRuntime.jsx("path", { d: "M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z", stroke: fill ?? "#FF0000", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" }, void 0), jsxRuntime.jsx("path", { d: "M12 8V12", stroke: fill ?? "#FF0000", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" }, void 0), jsxRuntime.jsx("path", { d: "M12 16H12.01", stroke: fill ?? "#FF0000", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" }, void 0)] }, void 0));
 }
+var icons = { AlertCicle };
 
 /**
  * @param {UserCardProps} props
@@ -936,7 +937,7 @@ function TextField(props) {
                             : jsxRuntime.jsx("span", { children: props.endIcon }, void 0))] }, void 0), props.helperText && jsxRuntime.jsx(HelperText, { children: props.helperText }, void 0)] }, void 0) }, void 0));
 }
 
-exports.AlertCicleIcon = AlertCicle;
+exports.AlertCicle = icons;
 exports.BaseCard = EmptyCard;
 exports.Button = Button;
 exports.CalendarCard = CalendarCard;
