cf585a0dc54df3b40537bfdd82d15ab57ab48836
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2022-05-16 18:35:51 -0300

    v0.2.12

--- dist/src/components/tag/index.d.ts
+++ dist/src/components/tag/index.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="react" />
 import '../../shared/global.css';
+declare type iconStatus = 'warning' | 'checked';
 interface TagProps {
     title: string;
     color: string;
     loading?: boolean;
     selected: boolean;
     inverted: boolean;
+    iconType?: iconStatus;
 }
 export default function Tag(props: TagProps): JSX.Element;
 export {};
