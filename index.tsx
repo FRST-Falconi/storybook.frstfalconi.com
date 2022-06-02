4cd33d0c82d7e98c712f13586a284408c49d8971
Lucas Santos <fclucasnet@gmail.com>
2022-06-02 12:11:27 -0300

    feat: new avatar component

--- index.tsx
+++ index.tsx
@@ -13,5 +13,8 @@ export { default as CalendarCard } from "./src/components/calendar"
 export { default as Button } from "./src/components/buttons"
 
 export { default as TextField } from './src/components/form-elements/textfield'
+export { default as TextArea } from './src/components/form-elements/textarea'
 
 export { AlertCicle, DoubleCheck } from './src/shared/icons'
+
+export { default as Avatar } from './src/components/avatar'
---
+++ src/components/avatar/avatar.d.ts
@@ -0,0 +1,10 @@
+import React from "react"
+declare module '*.png'
+
+export interface IAvatar {
+    size?: string,
+    src?: any,
+    alt?: string,
+    className?: string,
+    disabled?: boolean,
+}
