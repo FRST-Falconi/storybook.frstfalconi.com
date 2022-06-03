1b4e4ff93a4e51aeca33f06a48efb27d2c2af5f5
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2022-06-03 19:23:26 -0300

    feat: new icon more dots in horizontal

--- index.tsx
+++ index.tsx
@@ -15,6 +15,6 @@ export { default as Button } from "./src/components/buttons"
 export { default as TextField } from './src/components/form-elements/textfield'
 export { default as TextArea } from './src/components/form-elements/textarea'
 
-export { AlertCicle, DoubleCheck } from './src/shared/icons'
+export { AlertCicle, DoubleCheck, MoreDotsHorizontal } from './src/shared/icons'
 
 export { default as Avatar } from './src/components/avatar'
