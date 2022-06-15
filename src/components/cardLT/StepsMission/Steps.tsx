5c5f2ad6ca10189cf0392e6ea83ba608e7d18d51
Pedro Muniz <pedroalvesmuniz@gmail.com>
2022-06-15 15:58:26 -0300

    Teste de Tag

--- src/components/cardLT/StepsMission/Steps.tsx
+++ src/components/cardLT/StepsMission/Steps.tsx
@@ -15,15 +15,15 @@ interface StepParams {
   /**
    * @prop {number} step: Step (Marte, Júpter, Saturno, Urano, Netuno)
    */
-   step: number;
+   step: number
   /**
    * @prop {string} status: Status Planeta (B- Bloqueado, I- Inativo e A- Ativo)
    */
-   status : string;
+   status : string
   /**
    * @prop {React.CSSProperties} style: Styles de CSS adicional
    */  
-  style?: React.CSSProperties;
+  style?: React.CSSProperties
   /**
    * @prop {() => void} onClick: Função que será executada quando houver o click do botão
    */
