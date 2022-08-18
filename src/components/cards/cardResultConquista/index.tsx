916ca779f5f739086ce9196cf2bc61858be4f32e
Marlon <marlonps94@gmail.com>
2022-08-18 11:40:04 -0300

    add card de resultados e conquistas (#52)

---
+++ src/components/cards/cardResultConquista/index.tsx
@@ -0,0 +1,65 @@
+import Avatar from '@components/avatar'
+import '../../../shared/global.css'
+import {FRSTTheme} from '../../../theme'
+import style from './cardResultConquista.module.css'
+import Conquista from './Icons/Conquista.png'
+import ConquistaPressed from './Icons/ConquistaPressed.png'
+import Aprendizado from './Icons/Aprendizado.png'
+import AprendizadoPressed from './Icons/AprendizadoPressed.png'
+import { ApiRounded } from '@mui/icons-material'
+import { useState } from 'react'
+
+interface CardResultConquistaProps {
+    userAvatar ?: string
+    /**
+        * @prop {number} statusCard: Qual card será exibido: 1 - Conquista, 2 - Aprendizado
+    */
+    statusCard: number
+    userName: string
+    userArea: string
+    description: string
+    problemId: string
+
+    onClick?: (problemId: string) => void
+}
+
+
+/**
+ * @param {CardResultConquistaProps} props
+ */
+export default function CardResultConquista(props: CardResultConquistaProps) {
+    
+    const [isPressed, setIsPressed] = useState(false)
+    const descricao = props.description
+    return (
+        <div className={style.container} style={{cursor:'pointer', backgroundColor: isPressed ? '#FF4D0D' : '#FFF'}} onClick={() => 
+            {
+                props.onClick(props.problemId)
+                setIsPressed(true)
+            }}>
+            <div className={style.cardAvatar}>
+                <Avatar size='50px' src={props.userAvatar} />
+                <span>&nbsp;&nbsp;</span>
+                {props.statusCard === 1 ?
+                    isPressed ? 
+                        <img src={ConquistaPressed} alt="Icone de conquista" />
+                        :
+                        <img src={Conquista} alt="Icone de conquista" />
+                    :
+                    isPressed ?
+                        <img src={AprendizadoPressed} alt="Icone de aprendizado" />
+                        :
+                        <img src={Aprendizado} alt="Icone de aprendizado" />
+                }
+            </div>
+            
+            <span style={{color: isPressed ? '#FFF' : '#222', fontWeight: 600, fontSize: 16, marginTop: 12}}>{props.userName}</span>
+            <span style={{color: isPressed ? '#FFF' : '#222', fontWeight: 400, fontSize: 12, marginTop: 4}}>{props.userArea}</span>
+           
+            <div className={style.description} style={{color: isPressed ? '#FFD600' : '#FF4D0D',fontWeight: 500, fontSize: 16, textAlign:'center', marginTop: 12}}>
+                {descricao}
+            </div>
+            <div className={style.verMais}>Mais detalhes</div>
+        </div>
+    )
+}
