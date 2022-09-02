0156e5c3d7b9ab3b45bba8f99171af4022168e96
Marlon Pereira Silveira <marlonps94@gmail.com>
2022-09-02 15:50:27 -0300

    generate build

---
+++ dist/src/components/progressBar/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import '../../shared/global.css';
+import { IProgressBar } from './progressBar';
+export default function ProgressBar({ value }: IProgressBar): JSX.Element;
+//# sourceMappingURL=index.d.ts.map
