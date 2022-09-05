92445025065858c852d817c81dce74cea73014e8
Suzanne Patrícia de Souza <93949658+SuzannePS@users.noreply.github.com>
2022-09-05 16:11:46 -0300

    ajustes colorPicker

--- src/components/colorpicker/index.tsx
+++ src/components/colorpicker/index.tsx
@@ -3,18 +3,22 @@ import { ThemeProvider } from 'styled-components'
 import { FRSTTheme } from '../../theme'
 import * as Styles from './colorpickerStyles'
 import { IColorpicker } from './colorpicker'
-import { useColor } from 'react-color-palette'
 import 'react-color-palette/lib/css/styles.css'
-import { useState } from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 export default function Colorpicker({ width, height, className, disabled, color, setColor}: IColorpicker) {
-    const[cor,setCor]= useState(color)
-  return (
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(
+
     <ThemeProvider theme={FRSTTheme}>
-        <Styles.Container>
-        <Styles.ImputBox type="text" value= {cor} onChange={()=>setCor} color={cor} />
-        <Styles.ImputColor type="color" value={cor} onChange={()=>setCor} color={cor} />
-       </Styles.Container>
+        ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+        rootElement
+        );
     </ThemeProvider>
   )
 }
