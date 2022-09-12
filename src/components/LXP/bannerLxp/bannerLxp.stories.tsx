74b48cfdf94fcf2b6f44df17f129d9eb2b938f39
Marlon Pereira Silveira <marlonps94@gmail.com>
2022-09-12 17:04:49 -0300

    Started Banner LXP

---
+++ src/components/LXP/bannerLxp/bannerLxp.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import BannerLxp from './index'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'LXP/BannerLxp',
+    component: BannerLxp,
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <BannerLxp {...args} />
+
+export const Normal = Template.bind({})
+Normal.args = {
+    title: 'Nome do seu KnowHub',
+    bgColor: '#5e5e5e',
+    isDisabledText: true
+}
+
--- src/components/LXP/bannerLxp/index.tsx
+++ src/components/LXP/bannerLxp/index.tsx
@@ -1,12 +1,34 @@
-
+import Button from '@components/buttons'
+import TextField from '@components/form-elements/textfield'
+import style from './bannerLxp.module.css'
 interface BannerLxpParams{
     title: string,
-    
+    titleColor?: string,
+    bgSrc?: string,
+    bgColor?: string,
+    isDisabledText?: boolean,
+    style?: React.CSSProperties
 }
 
 export default function BannerLxp ( props : BannerLxpParams) {
+
+    function configBanner (){
+        return(
+            <div className={style.configContainer}>
+                <span>Título</span>
+                <span>Alterar título do KnowHub</span>
+                <TextField />
+            </div>
+        )
+    }
+
     return (
-        <>
-        </>
+        <div className={style.bannerContainer} style={{...props.style,backgroundColor: props.bgColor }} >
+            {!props.isDisabledText ?
+                <span style={{color: props.titleColor ? props.titleColor : '#FFF', fontSize: 40, fontWeight: 700 }}>{props.title}</span>
+                : ''
+            }
+            <div className={style.configButton}> <Button variant='primary' label='Configuração de capa' handleClick={ () => configBanner} /> </div>
+        </div>
     )
 }
