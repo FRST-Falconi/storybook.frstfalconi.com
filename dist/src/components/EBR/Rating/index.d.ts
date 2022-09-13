4f1b16f3ffba1efe067d4ad049928d7db7c584d6
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2022-09-13 16:14:42 -0300

    build 0.2.83

--- dist/src/components/EBR/Rating/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { IRating } from './rating';
-/**
- * @componente
- */
-export default function RatingFrst({ rating, isVisibleNumberRating, handleRating, size }: IRating): JSX.Element;
-//# sourceMappingURL=index.d.ts.map
---
+++ dist/src/components/EBR/objective-step/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IObjectiveStep } from './objectiveStep';
+export default function ObjectiveStep({ description, number }: IObjectiveStep): JSX.Element;
+//# sourceMappingURL=index.d.ts.map
