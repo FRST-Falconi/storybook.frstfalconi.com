d33ae3c3a974015964b5b66e689c3edbadd2cc13
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2022-09-16 12:14:49 -0300

    feat: select component

---
+++ src/components/select/SelectContext.tsx
@@ -0,0 +1,9 @@
+// Create react context to share data between components
+
+import React from 'react'
+
+interface ContextProps {
+    selected?: boolean
+}
+
+export const SelectContext = React.createContext<ContextProps | null>(null)
