fc1a2fc41ffe7ae505b334c41cfce677b74a533d
Pedro Muniz <pedroalvesmuniz@gmail.com>
2022-09-30 15:30:25 -0300

    Add traducao para componentes steps mission (#111)

--- src/components/cardLT/StepsMission/StepMission.tsx
+++ src/components/cardLT/StepsMission/StepMission.tsx
@@ -14,7 +14,8 @@ interface MissionStepsParams {
   /**
    * @prop {number} stepActive: 
    */
-   stepActive : number;
+   stepActive : number
+   idioma? : 'pt-BR' | 'pt-PT' | 'en-US' | 'es'
   /**
    * @prop {React.CSSProperties} style: Styles de CSS adicional
    */  
@@ -38,11 +39,16 @@ export default function MissionSteps(props: MissionStepsParams) {
 
   const [stepLiberado, setstepLiberado] = useState(props.stepProblem);
   const [stepActive, setStepActive] = useState(props.stepActive);
+  const [Idioma, setIdioma] = useState(props.idioma ? props.idioma : 'pt-BR');
 
   const setStep = (step: number) => {
     setStepActive(step)
     props.onSelected(step)
   }
+
+  useEffect(() => {
+    setIdioma(props.idioma ? props.idioma : 'pt-BR')
+  }, [props.idioma])
 
   useEffect(() =>{
     setstepLiberado(props.stepProblem)
@@ -86,6 +92,7 @@ export default function MissionSteps(props: MissionStepsParams) {
         <div style={{display:"inline-flex", marginTop: 40, justifyContent: 'center', width: '100%'}}>
           <Steps
             step={1}
+            idioma={Idioma}
             status={ stepLiberado >= 1 ? stepActive === 1 ? "A" : "I" : "B"  } 
             onClick={() => {
               setStep(1)
@@ -94,6 +101,7 @@ export default function MissionSteps(props: MissionStepsParams) {
           />
           <Steps
             step={2}
+            idioma={Idioma}
             status={ stepLiberado >= 2 ? stepActive === 2 ? "A" : "I" : "B"  } 
             onClick={() => {
               setStep(2)
@@ -102,6 +110,7 @@ export default function MissionSteps(props: MissionStepsParams) {
           />
           <Steps
             step={3}
+            idioma={Idioma}
             status={ stepLiberado >= 3 ? stepActive === 3 ? "A" : "I" : "B"  } 
             onClick={() => {
               setStep(3)
@@ -110,6 +119,7 @@ export default function MissionSteps(props: MissionStepsParams) {
           />
           <Steps
             step={4}
+            idioma={Idioma}
             status={ stepLiberado >= 4 ? stepActive === 4 ? "A" : "I" : "B"  } 
             onClick={() => {
               setStep(4)
@@ -118,6 +128,7 @@ export default function MissionSteps(props: MissionStepsParams) {
           />
           <Steps
             step={5}
+            idioma={Idioma}
             status={ stepLiberado >= 5 ? stepActive === 5 ? "A" : "I" : "B"  } 
             onClick={() => {
               setStep(5)
--- src/components/cardLT/StepsMission/Steps.tsx
+++ src/components/cardLT/StepsMission/Steps.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import style from './Planet.module.css'
 
@@ -14,6 +14,7 @@ interface StepParams {
    * @prop {string} status: Status Planeta (B- Bloqueado, I- Inativo e A- Ativo)
    */
    status : string
+   idioma? : 'pt-BR' | 'pt-PT' | 'en-US' | 'es'
   /**
    * @prop {React.CSSProperties} style: Styles de CSS adicional
    */  
@@ -43,7 +44,7 @@ export default function Steps(props: StepParams) {
     'https://api-motor.s3.amazonaws.com/netuno.png',
   ]
 
-  const TypeStepName = [
+  const TypeStepNamePTBR = [      
     'Marte',
     'Júpiter',
     'Saturno',
@@ -51,8 +52,43 @@ export default function Steps(props: StepParams) {
     'Netuno',
   ]
 
-  return (
+  const TypeStepNamePT = [      
+    'Marte',
+    'Júpiter',
+    'Saturno',
+    'Urano',
+    'Netuno',
+  ]
 
+  const TypeStepNameEN = [      
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptuno',
+  ]
+
+  const TypeStepNameES = [      
+    'Marte',
+    'Júpiter',
+    'Saturno',
+    'Urano',
+    'Netuno',
+  ]
+
+  const mapTraducao = new Map()
+  mapTraducao.set("pt-BR", TypeStepNamePTBR)
+  mapTraducao.set("pt-PT", TypeStepNamePT)
+  mapTraducao.set("en-US", TypeStepNameEN)
+  mapTraducao.set("es", TypeStepNameES)
+
+  const [Idioma, setIdioma] = useState(props.idioma ? props.idioma : 'pt-BR');
+
+  useEffect(() => {
+    setIdioma(props.idioma ? props.idioma : 'pt-BR')
+  }, [props.idioma])
+
+  return (
     <>    
       <div className={style.container} style={{...props.style}} onClick={() => {
         if (props.status !== 'B') {
@@ -66,7 +102,7 @@ export default function Steps(props: StepParams) {
               : props.status === "I" ? 
                 style.imgInative 
               : style.imgBlocked} />
-        <div className={style.label}>{TypeStepName[props.step-1]}</div>
+        <div className={style.label}>{mapTraducao.get(Idioma)[props.step-1]}</div>
         {
           props.status === 'B' &&        
           <div className={style.block}>
@@ -76,6 +112,5 @@ export default function Steps(props: StepParams) {
       </div>
     </>
   )
-
 }
 
