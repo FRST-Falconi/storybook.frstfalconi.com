04465aa606f0ed85779db30789551808f8e65cf3
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2022-10-07 10:34:58 -0300

    fix: margin button commentary box component (#127)
    * fix: margin button commentary box component
    
    * feat: build version 0.3.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+export { AlertCicle, DoubleCheck, MoreDotsHorizontal, CheckInCicle, HomeLineIcon, HomeFilledIcon, CalendarLineIcon, CalendarFilledIcon, PencilLineIcon, PencilFilledIcon, LampLineIcon, LampFilledIcon, MedalLineIcon, MedalFilledIcon, PlayLineIcon, PlayFilledIcon } from './src/shared/icons';
 export { default as Tag } from "./src/components/tag";
 export { default as BaseCard } from "./src/components/cards/base";
 export { default as UserCard } from "./src/components/cards/user";
@@ -8,7 +9,6 @@ export { default as Button } from "./src/components/buttons";
 export { default as ButtonGroup } from "./src/components/EBR/buttons-group";
 export { default as TextField } from './src/components/form-elements/textfield';
 export { default as TextArea } from './src/components/form-elements/textarea';
-export { AlertCicle, DoubleCheck, MoreDotsHorizontal, CheckInCicle } from './src/shared/icons';
 export { default as Avatar } from './src/components/avatar';
 export { default as InputComment } from './src/components/input-comment';
 export { default as CommentaryBox } from './src/components/commentary-box';
---
+++ dist/src/components/item-lateral-menu/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import '../../shared/global.css';
+import { IItemLateralMenu } from './itemLateralMenu';
+export default function LateralMenu({ label, onClick, button, beginComponent, activeBeginIcon, selected, subItems, hiddenIconDropArrow, style }: IItemLateralMenu): JSX.Element;
+//# sourceMappingURL=index.d.ts.map
