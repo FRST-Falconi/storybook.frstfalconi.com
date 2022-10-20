c645c7f4ca5b440eac92af0d417a6a6c382d3b49
Marlon Pereira Silveira <marlonps94@gmail.com>
2022-10-20 17:54:13 -0300

    Fix Icons fill prop and mobile menu width and height

--- src/components/global-menu/globalMenuStyles.ts
+++ src/components/global-menu/globalMenuStyles.ts
@@ -82,8 +82,8 @@ export const MenuMobile = styled.div<{isVisible}>`
     z-index: 10001;
     top:0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     background: #222;
 `
 
