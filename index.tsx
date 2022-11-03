31b84a0635b803c75fff16e224f4e0fb96b39d09
biaGomesFrst <biancamoraes@frstfalconi.com>
2022-11-03 09:30:31 -0400

    exportar o login lxp

--- index.tsx
+++ index.tsx
@@ -1,25 +1,46 @@
 // EXPORT COMPONENTS HERE
 
 // Icons
-export { AlertCicle, DoubleCheck, MoreDotsHorizontal, CheckInCicle, TrashIcon,
-        HomeLineIcon, HomeFilledIcon, CalendarLineIcon, CalendarFilledIcon, PencilLineIcon, PencilFilledIcon,
-        LampLineIcon, LampFilledIcon, MedalLineIcon, MedalFilledIcon, PlayLineIcon, PlayFilledIcon,
-        IconNotification, InstagramIcon, LinkedinIcon, PodCastIcon, SiteIcon, SpotifyIcon, YoutubeIcon  } from './src/shared/icons'
+export {
+  AlertCicle,
+  DoubleCheck,
+  MoreDotsHorizontal,
+  CheckInCicle,
+  TrashIcon,
+  HomeLineIcon,
+  HomeFilledIcon,
+  CalendarLineIcon,
+  CalendarFilledIcon,
+  PencilLineIcon,
+  PencilFilledIcon,
+  LampLineIcon,
+  LampFilledIcon,
+  MedalLineIcon,
+  MedalFilledIcon,
+  PlayLineIcon,
+  PlayFilledIcon,
+  IconNotification,
+  InstagramIcon,
+  LinkedinIcon,
+  PodCastIcon,
+  SiteIcon,
+  SpotifyIcon,
+  YoutubeIcon
+} from './src/shared/icons'
 
 // Base
-export { default as Tag } from "./src/components/tag"
-export { default as BaseCard } from "./src/components/cards/base"
-
+export { default as Tag } from './src/components/tag'
+export { default as BaseCard } from './src/components/cards/base'
 
 // CARDS
-export { default as UserCard } from "./src/components/cards/user"
-export { default as CalendarCard } from "./src/components/calendar"
-export { default as BannerProblem } from "./src/components/cardLT/BannerProblem"
-export { default as MessageBox } from "./src/components/cardLT/MessageBox"
+export { default as UserCard } from './src/components/cards/user'
+export { default as CalendarCard } from './src/components/calendar'
+export { default as BannerProblem } from './src/components/cardLT/BannerProblem'
+export { default as MessageBox } from './src/components/cardLT/MessageBox'
 
 // Buttons
-export { default as Button } from "./src/components/buttons"
-export { default as ButtonGroup } from "./src/components/EBR/buttons-group"
+export { default as Button } from './src/components/buttons'
+export { default as ButtonGroup } from './src/components/EBR/buttons-group'
 export { default as TextField } from './src/components/form-elements/textfield'
 export { default as TextArea } from './src/components/form-elements/textarea'
 export { default as Avatar } from './src/components/avatar'
@@ -30,7 +51,7 @@ export { default as ScrollContainer } from './src/components/scroll-container'
 export { default as LearningSteps } from './src/components/learning-steps'
 export { default as TotalizerCard } from './src/components/totalizer-card'
 
-export { default as Select } from "./src/components/DS/select"
+export { default as Select } from './src/components/DS/select'
 
 export { default as SearchField } from './src/components/search-field'
 export { default as FilterAccordionCheckbox } from './src/components/filter-accordion-checkbox'
@@ -41,13 +62,12 @@ export { default as ModalLearningTech } from './src/components/modal/modalLearni
 export { default as CardProblemGestor } from './src/components/cards/cardProblemGestor'
 export { default as CardProblem } from './src/components/cards/cardProblem'
 export { default as CardDefinicaoProblema } from './src/components/cards/cardDefinicaoProblema'
-export { default as CardResultConquista} from './src/components/cards/cardResultConquista'
+export { default as CardResultConquista } from './src/components/cards/cardResultConquista'
 
 export { default as ExclusiveClassCard } from './src/components/exclusive-class'
-export { default as ConquistaCarrossel} from './src/components/conquistaCarrossel'
-export { default as Steps} from './src/components/cardLT/StepsMission/Steps'
-export { default as StepsMission} from './src/components/cardLT/StepsMission/StepMission'
-
+export { default as ConquistaCarrossel } from './src/components/conquistaCarrossel'
+export { default as Steps } from './src/components/cardLT/StepsMission/Steps'
+export { default as StepsMission } from './src/components/cardLT/StepsMission/StepMission'
 
 export { default as Rating } from './src/components/EBR/rating'
 
@@ -60,26 +80,27 @@ export { default as GroupStatistics } from './src/components/EBR/group-statistic
 export { default as TrailList } from './src/components/EBR/trail-list'
 
 // LXP
-export { default as avatarChannel} from './src/components/LXP/avatarChannel'
-export { default as bannerLxp} from './src/components/LXP/bannerLxp'
-export { default as buttonContent} from './src/components/LXP/buttonContent'
-export { default as colorPicker} from './src/components/LXP/colorpicker'
-export { default as contentSwitcher} from './src/components/LXP/contentSwitcher'
-export { default as contentThumbnails} from './src/components/LXP/contentThumbnails'
-export { default as fileUpload} from './src/components/LXP/fileUpload'
-export { default as headerContent} from './src/components/LXP/headerContent'
-export { default as popOver} from './src/components/LXP/popOver'
-export { default as progressBar} from './src/components/LXP/progressBar'
-export { default as searchResults} from './src/components/LXP/searchResults'
-export { default as starMetric} from './src/components/LXP/star-metric'
-export { default as CalendarLxp} from './src/components/LXP/calendarLxp'
-export { default as Video} from './src/components/LXP/video'
-export { default as GlobalMenu} from './src/components/global-menu'
-export { default as extraContent} from './src/components/LXP/extraContent'
+export { default as avatarChannel } from './src/components/LXP/avatarChannel'
+export { default as bannerLxp } from './src/components/LXP/bannerLxp'
+export { default as buttonContent } from './src/components/LXP/buttonContent'
+export { default as colorPicker } from './src/components/LXP/colorpicker'
+export { default as contentSwitcher } from './src/components/LXP/contentSwitcher'
+export { default as contentThumbnails } from './src/components/LXP/contentThumbnails'
+export { default as fileUpload } from './src/components/LXP/fileUpload'
+export { default as headerContent } from './src/components/LXP/headerContent'
+export { default as popOver } from './src/components/LXP/popOver'
+export { default as progressBar } from './src/components/LXP/progressBar'
+export { default as searchResults } from './src/components/LXP/searchResults'
+export { default as starMetric } from './src/components/LXP/star-metric'
+export { default as CalendarLxp } from './src/components/LXP/calendarLxp'
+export { default as Video } from './src/components/LXP/video'
+export { default as GlobalMenu } from './src/components/global-menu'
+export { default as extraContent } from './src/components/LXP/extraContent'
+export { default as LoginLxp } from './src/components/LXP/login'
 
-// FI 
-export { default as CardTrail} from './src/components/card-trail'
-export { default as CardsTrailsCarousel} from './src/components/card-trail-carousel'
+// FI
+export { default as CardTrail } from './src/components/card-trail'
+export { default as CardsTrailsCarousel } from './src/components/card-trail-carousel'
 export { default as ChallengeCard } from './src/components/challenge-cards'
 export { default as SearchBox } from './src/components/FI/searchBox'
 export { default as BannerSRG } from './src/components/FI/bannerSRG'
