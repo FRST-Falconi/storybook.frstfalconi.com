5d17dd41b59957852f39d9ebc29d9092fecdd039
Sergio Henrique Silveira Costa e Leôncio <94614482+sergiohscl@users.noreply.github.com>
2022-11-30 12:45:26 -0300

    importação do accordionTrack (#198)

---
+++ dist/src/components/LXP/accordionTrack/accordionTrackList.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import '../../../shared/global.css';
+import { IAccordionTranslate } from './IAccordionTrack';
+export default function AccordionTrackList({ trailsData, handleChange, onNewTrail, textMeusConteudos, textTotalDe, textRegistros, textMinhasTrihas, txtAtivarCurso, txtButtonLabel, txtCriarNovoCurso }: IAccordionTranslate): JSX.Element;
+//# sourceMappingURL=accordionTrackList.d.ts.map
---
+++ dist/src/components/LXP/accordionTrack/vectorUp.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+interface VectorIconParam {
+    color?: string;
+}
+/**
+ *
+ * @componente
+ */
+export default function VectorUp(props: VectorIconParam): JSX.Element;
+export {};
+//# sourceMappingURL=vectorUp.d.ts.map
