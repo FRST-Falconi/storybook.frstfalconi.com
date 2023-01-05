642567f54f42408551a4d6c31e4f43699a964bfc
Sergio Henrique Silveira Costa e Leôncio <94614482+sergiohscl@users.noreply.github.com>
2023-01-05 13:16:49 -0300

    Fix/remove dropdown select multiple (#243)
    * fix: remove dropdown select multiple
    
    * build

--- dist/src/components/LXP/DropdownSelectMultiple/styles/MultiSelectDemo.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const SelectMultipesPrimeReact: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const SelectMultipesLabel: import("styled-components").StyledComponent<"label", any, {}, never>;
-//# sourceMappingURL=MultiSelectDemo.d.ts.map
