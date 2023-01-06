2b0e39fcc3bd13fc4b7ef74716a9d774fbdc67d7
biaGomesFrst <biancamoraes@frstfalconi.com>
2023-01-06 11:23:52 -0300

    build 0.4.6

---
+++ dist/src/components/LXP/DropdownSelectMultiple/styles/MultiSelectDemo.d.ts
@@ -0,0 +1,3 @@
+export declare const SelectMultipesPrimeReact: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const SelectMultipesLabel: import("styled-components").StyledComponent<"label", any, {}, never>;
+//# sourceMappingURL=MultiSelectDemo.d.ts.map
