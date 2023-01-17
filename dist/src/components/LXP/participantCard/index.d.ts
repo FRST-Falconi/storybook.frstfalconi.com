31ec6fa097695a9777c398284802596d92acfc20
Marlon <marlonps94@gmail.com>
2023-01-17 15:45:24 -0300

    Fix add style props at components (#262)
    * Fix add style props at components
    
    * build 0.6.4

--- dist/src/components/LXP/participantCard/index.d.ts
+++ dist/src/components/LXP/participantCard/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
 import '../../../shared/global.css';
 import { ParticipantCardI } from './participantCard';
-export default function ParticipantCard({ userInfo, labels, handleSendInvitation, handleClickRemove }: ParticipantCardI): JSX.Element;
+export default function ParticipantCard({ userInfo, labels, style, handleSendInvitation, handleClickRemove }: ParticipantCardI): JSX.Element;
 //# sourceMappingURL=index.d.ts.map
