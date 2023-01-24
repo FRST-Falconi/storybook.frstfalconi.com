8da027a730c7d85023002f9a3153868d6a3522b6
sergiohscl <sergiohscl2@gmail.com>
2023-01-24 15:22:41 -0300

    ajustes no componente contentSwitcher

--- src/components/LXP/contentSwitcher/contentSwitcherStyle.ts
+++ src/components/LXP/contentSwitcher/contentSwitcherStyle.ts
@@ -35,7 +35,7 @@ export const ContentSwitcher= styled.button<{}>`
     align-items: center;
     justify-content: center;
     text-align: center;
-    padding: 16px;
+    padding: 20px;
     width: 180px;
     height: 176px;
     border: 1px solid ${({ theme }) => theme.colors.neutralsGrey5};
