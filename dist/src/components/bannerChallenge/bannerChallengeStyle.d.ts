5e79b1e1bc5ef353aaaf0794fc798db02cfc705e
luizpromkt <luizalbertovjr@gmail.com>
2023-01-31 19:17:55 -0300

    build

--- dist/src/components/bannerChallenge/bannerChallengeStyle.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const Div: import("styled-components").StyledComponent<"div", any, any, never>;
-//# sourceMappingURL=bannerChallengeStyle.d.ts.map
--- dist/src/components/bannerChallenge/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import '../../shared/global.css';
-interface BannerChallengeProps {
-    label?: string;
-}
-export default function BannerChallenge({ label }: BannerChallengeProps): JSX.Element;
-export {};
-//# sourceMappingURL=index.d.ts.map
