983ad9f02e086694204e3ed4df49b9df55a54da3
Lucas Santos <fclucasnet@gmail.com>
2023-02-09 14:45:44 -0300

    fix: specialistcontact spacing (#284)
    * fix: specialistcontact spacing
    
    * build

--- src/components/EBR/specialist-contact/specialistContactStyles.ts
+++ src/components/EBR/specialist-contact/specialistContactStyles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div<{mobileVersion: boolean}>`
     display: flex;
     flex-direction: column;
-    max-width:  ${({mobileVersion})=> mobileVersion ? '343px': '375px'};
+    max-width:  ${({mobileVersion})=> mobileVersion ? '343px': '400px'};
 `
 
 export const Title = styled.p<{mobileVersion: boolean}>`
@@ -89,4 +89,5 @@ export const WrapperButtons = styled.div`
     justify-content: space-between;
     width: 100%;
     margin-top: 45px;
+    gap: 16px;
 `
