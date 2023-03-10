1d9c2a29d80423aee56e38b021fc088304cf17b5
Lucas Santos <fclucasnet@gmail.com>
2023-03-10 14:33:12 -0300

    feat: banner problem verified status

--- src/components/cardLT/BannerProblem/BannerProblem.d.ts
+++ src/components/cardLT/BannerProblem/BannerProblem.d.ts
@@ -5,9 +5,9 @@ import React from "react"
 interface IBannerProblemParams {
     /**
      * @prop {React.CSSProperties} style: Styles de CSS adicional
-     */  
+     */
     style?: React.CSSProperties
-    
+
     company?: string,
     topHeaderTagColor?: string
     topHeaderTagBgColor?: string
@@ -31,27 +31,29 @@ interface IBannerProblemParams {
     descriptionRelevancia: string
     curtidas?: number
     stepProblem: number
-    stepActive: number            
+    stepActive: number
+    isVerified?: boolean
+    verifiedTooltipContent?: any
     onSelectedStep: (step: number) => void
-    onClickSave:([]) => void 
+    onClickSave:([]) => void
     /**
      * @prop {object} trilhaData: A listagem de Trilhas no Select [{label: 'trilha1', value: 'id1'}]
-     */  
+     */
     trilhaData?: any
     /**
      * @prop {string} trilha: Descrição da Trilha Selecionada
-     */  
+     */
     trilha: string
     /**
      * @prop {string} trilhaId: Id da Trilha Selecionada, que será usado para selecionar o select quando for editar
-     */    
+     */
     trilhaId?: any
     isEditable?: boolean
     isVisibleEditTrail?: boolean
     isVisibleEditTags?: boolean
     /**
      * @prop {object} tagData: A listagem de Tags no Select [{label: 'TAG1', value: 'id1'}]
-     */    
+     */
     tagData?: any
     children: React.ReactNode
     onClickMessage: () => void
@@ -60,30 +62,30 @@ interface IBannerProblemParams {
     */
     onClickContinue?: () => void
   }
-  
+
   ///-----------------------------------------
   /// Componente
-  
-  
-  
+
+
+
   /**
-   * 
-   * @componente 
+   *
+   * @componente
    */
 
 export interface IBannerProgressTranslate  extends IBannerProblemParams{
     textTrailLabel ?: string
     textTitleProblem?: string
-    textButtonLinkEdit?: string 
+    textButtonLinkEdit?: string
     textButtonLinkEditSave?: string
     placeholderSelectTrail?: string
     textIconDescription?: string
     textContentInput?: string
     placeholderSelectTag?: string
-    missionIdioma?: 'pt-BR' | 'pt-PT' | 'en-US' | 'es' 
-    ratingTitleImpact?: string 
-    ratingTitleRelevance?: string 
-    ratingTitleLikes?: string 
+    missionIdioma?: 'pt-BR' | 'pt-PT' | 'en-US' | 'es'
+    ratingTitleImpact?: string
+    ratingTitleRelevance?: string
+    ratingTitleLikes?: string
     ratingLikesEvaluation?: string
     ratingLikesDescription?: string
     ratingNavegationAnt?: string
