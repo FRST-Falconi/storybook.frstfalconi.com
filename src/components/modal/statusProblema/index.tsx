8c0dc6678dda7be1b2e3a4470fed84fd8002f36f
Lucas Santos <fclucasnet@gmail.com>
2023-03-17 15:26:09 -0300

    feat: add new design (#321)
    * feat: add new design
    
    * build
    
    * fix: fix flex style
    
    * build
    
    * build:0.12.6

--- src/components/modal/statusProblema/index.tsx
+++ src/components/modal/statusProblema/index.tsx
@@ -1,14 +1,17 @@
 import { useState, useLayoutEffect } from 'react'
-import { Grid, Box, Modal } from '@mui/material'
+import { Box, Modal } from '@mui/material'
 import style from './statusProblema.module.css'
 import Button from '../../buttons/index'
 import Avatar from '../../avatar/index'
-import problemaFRST from './problemaFRST.png'
-import FecharIcon from './fecharIcon'
-import { CorrectInCicleIcon, WarningInCicleIcon, ErrorInCicleIcon} from '../../../shared/icons'
-import {AlertCicle, CloseIcon} from '../../../shared/icons'
+import problemaFRST from './problemaFRST.png'   
+import {
+    StatusProblemaSawBadgeIcon,
+    StatusProblemaSendIcon,
+    StatusProblemaEdit
+} from '../../../shared/icons'
+import {CloseIcon} from '../../../shared/icons'
 
-type enumStatus = 'aprovar' | 'revisar' | 'aguardar'
+type enumStatus = 'guest' | 'reviewed' | 'adjustments'
 
 interface ModalStatusProblemaProps {
     open: boolean,
@@ -27,33 +30,40 @@ interface ApprovesItemProps{
     dataAvatar: string,
     statusApprove: enumStatus,
     size: string
+    avatarSize: string
 }
 
 function ApprovesItem(props: ApprovesItemProps){
 
         return (
-            <div className={style.containerItem} >
-                <div className={style.avatar} style={{width: props.size}}>
-                    <Avatar src={props.avatar} size={props.size} />
-                    <span style={{ marginTop: 4, color: '#FF4D0D', fontWeight: 600 }}>{props.nomeAvatar}</span>
-                    <span style={{ fontSize: 14, color: '#222222', fontWeight: 400 }}>{props.dataAvatar}</span>
-                </div>
-                {
-                    props.statusApprove === 'aprovar' ?
-                    <div style={{minWidth: '48px'}}>
-                    <CorrectInCicleIcon/>
+            <>
+                <div className={style.containerItem}>
+                    <div className={style.avatar} style={{width: props.size}}>
+                        <Avatar src={props.avatar} size={props.avatarSize} />
                     </div>
-                    : props.statusApprove === 'revisar' ?
-                    <div style={{minWidth: '48px'}}>
-                    <ErrorInCicleIcon/>
+                    {
+                        props.statusApprove === 'reviewed' ?
+                        <div>
+                            <StatusProblemaSawBadgeIcon />
+                        </div>
+                        : props.statusApprove === 'adjustments' ?
+                        <div>
+                            <StatusProblemaEdit/>
+                        </div>
+                        : 
+                        <div><StatusProblemaSendIcon /></div>
+                    } 
+                    <div style={{width: props.size}} className={style.status_logo}>
+                        <Avatar src={problemaFRST} size={props.avatarSize}  />
                     </div>
-                    : 
-                    <div style={{minWidth: '48px'}}><WarningInCicleIcon/></div>
-                } 
-                <div style={{marginBottom: 30, width: props.size}}>
-                    <Avatar src={problemaFRST} size={props.size}  />
                 </div>
-            </div>
+                <div className={style.containerItem} style={{ marginTop: '8px' }}>
+                    <div className={style.containerData} style={{ width: props.size }}>
+                        <span className={style.name}>{props.nomeAvatar}</span>
+                        <span className={style.sub_title}>{props.dataAvatar}</span>
+                    </div>
+                </div>
+            </>
         )
     
 }
@@ -74,36 +84,40 @@ export default function ModalStatusProblema(props: ModalStatusProblemaProps) {
     const BREAKWIDTH = 500;
     
     return (
-        <Modal open={props.open} onClose={props.handleClose} >
-            
+        <Modal open={props.open} onClose={props.handleClose}>
             <Box className={style.modalBox} style={{top: '50%', left: '50%', transform: 'translate(-50%, -50%)', ...props.style}}>
                 <div className={style.fechar} onClick={props.handleClose}>
-                <span style= {{marginRight: 10}} > {props.language === 'en-US' ? 'Close' : 'Fechar' } </span> <CloseIcon />
+                    <CloseIcon />
                 </div>
                 <div className={style.titulo}>
                     {props.title}
                 </div>
-                <div className={style.container} style={{height: props.approves.length >= 3 ? 555 : 185 * props.approves.length}} >
-
-                    {
-                        props.approves.map((item, index) => (                 
-                            <ApprovesItem size={(size[0] >= BREAKWIDTH) ? '120px' : '80px'} nomeAvatar={item.nome} dataAvatar={item.data} statusApprove={item.approve} avatar={item.avatarFoto} key={index}  />
-                        ))
-                            
-                    }
-
+                <div className={style.container} style={{height: props.approves.length >= 3 ? 555 : 185 * props.approves.length}}>
+                    {props.approves.map((item, index) => (                 
+                        <ApprovesItem
+                            avatarSize={(size[0] >= BREAKWIDTH) ? '120px' : '100px'}
+                            size={(size[0] >= BREAKWIDTH) ? '160px' : '120px'}
+                            nomeAvatar={item.nome}
+                            dataAvatar={item.data}
+                            statusApprove={item.approve}
+                            avatar={item.avatarFoto}
+                            key={index}
+                        />
+                    ))}
                 </div>
-                <div className={style.descricao}>
-                    {props.description}
-                </div>
-                <div className={style.conclusao}>
-                    <Button 
-                        variant='primary'
-                        label={props.language === 'en-US' ? 'ok, close' : 'Ok, fechar'}
-                        handleClick={props.handleClose}
-                    />
-                </div>
-                        
+                <div className={style.modal_controls_wrapper}>
+                    <div className={style.descricao}>
+                        {props.description}
+                    </div>
+                    <div className={style.conclusao}>
+                        <Button 
+                            variant='primary'
+                            label={props.language === 'en-US' ? 'ok, close' : 'Ok, entendi'}
+                            handleClick={props.handleClose}
+                            style={{ width: '100%', minWidth: '140px' }}
+                        />
+                    </div>
+                </div> 
             </Box>
         </Modal>
     )
--- src/components/modal/statusProblema/statusProblema.stories.tsx
+++ src/components/modal/statusProblema/statusProblema.stories.tsx
@@ -15,14 +15,13 @@ export const Teste1 = Template.bind({})
 Teste1.args = {
   open: true,
   title: 'Parabéns, o seu problema foi aprovado!',
-  description: 'Você pode ter mais de uma pessoa aprovando ou não o seu problema e todos os resultados aparecerão aqui.',
+  description: 'Estas são as pessoas que foram convidadas para revisar se o objetivo deste desafio está alinhado às metas da empresa.  Caso esteja, ele constará como “verificado”.',
   approves:[
-    { nome: 'Marlon Pereira', data: '28 abr. de 2022',   approve: 'aprovar', avatar:'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg'  },
-    { nome: 'Marlon Pereira', data: '28 abr. de 2022',   approve: 'revisar', avatar:''  },
-    { nome: 'Marlon Pereira', data: '28 abr. de 2022',   approve: 'aguardar'  },
+    { nome: 'Marlon Pereira', data: 'Revisão concluída: desafio verificado',   approve: 'reviewed', avatar:'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg'  },
+    { nome: 'Marlon Pereira', data: 'Revisão concluída: desafio precisa de ajustes',   approve: 'adjustments', avatar:''  },
+    { nome: 'Marlon Pereira', data: 'Convite enviado: revisão ainda não realizada',   approve: 'guest'  },
    
   ],
   language: 'en-US'
 
 }
-
