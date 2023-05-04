6bfeaa64fee53def19c5abea48977019f436d726
biaGomesFrst <biancamoraes@frstfalconi.com>
2023-05-04 09:02:05 -0300

    build 0.15.6

--- dist/src/components/quantidadeDesafios/index.d.ts
+++ dist/src/components/quantidadeDesafios/index.d.ts
@@ -3,6 +3,8 @@ interface ObjItemDesafio {
     title: string;
     count: number;
     percentage: string;
+    title_en?: string;
+    title_es?: string;
 }
 interface ObjQuantidadeDesafio {
     definicao: ObjItemDesafio;
@@ -11,7 +13,8 @@ interface ObjQuantidadeDesafio {
     resultados: ObjItemDesafio;
     proximosPassos: ObjItemDesafio;
     finalizados: ObjItemDesafio;
+    languagem?: string;
 }
-export default function QuantidadeDesafios({ definicao, hipoteses, testes, resultados, proximosPassos, finalizados }: ObjQuantidadeDesafio): JSX.Element;
+export default function QuantidadeDesafios({ definicao, hipoteses, testes, resultados, proximosPassos, finalizados, languagem }: ObjQuantidadeDesafio): JSX.Element;
 export {};
 //# sourceMappingURL=index.d.ts.map
