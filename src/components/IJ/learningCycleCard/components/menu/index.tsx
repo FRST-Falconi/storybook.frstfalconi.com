c697e61094c10a4cecf16e5b399d3575d9483f0a
Lucas de Souza <lucasdesouza@MacBook-Air-de-Lucas.local>
2023-05-08 09:05:19 -0300

    feat: adjusts layou component

--- src/components/IJ/learningCycleCard/components/menu/index.tsx
+++ src/components/IJ/learningCycleCard/components/menu/index.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
@@ -5,6 +6,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert'
 import { useState } from 'react'
 import { Divider } from '@mui/material'
 import { TrashDelete } from '@shared/icons'
+import * as Styles from './menuStyle'
 
 export default function CardMenu({ id, menuOptions }) {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
@@ -31,37 +33,59 @@ export default function CardMenu({ id, menuOptions }) {
       </IconButton>
 
       {open && (
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            'aria-labelledby': 'long-button'
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              width: '300px'
-            }
-          }}
-        >
-          {menuOptions?.map((option, index) => (
-            <>
-              <MenuItem
-                key={index}
-                onClick={() => {
-                  handleClose(option)
-                }}
-                sx={{
-                  color: option?.type === 'delete' ? '#923534' : '#000000'
-                }}
-              >
-                {option?.type === 'delete' ? <TrashDelete fill="#923534" width="30px" /> : option?.icon} {option?.label}
-              </MenuItem>
-              <Divider />
-            </>
-          ))}
-        </Menu>
+        <>
+          <Styles.MenuIconOpen />
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              'aria-labelledby': 'long-button'
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                width: '300px',
+                position: 'relative'
+              }
+            }}
+            sx={{
+              '& .MuiMenu-paper': {
+                '&::before': {
+                  content: '""',
+                  position: 'absolute',
+                  top: '-20px',
+                  right: '10px',
+                  borderWidth: '10px',
+                  borderStyle: 'solid',
+                  borderColor: 'transparent transparent #fff transparent',
+                  zIndex: '9999'
+                }
+              }
+            }}
+          >
+            {menuOptions?.map((option, index) => (
+              <>
+                <MenuItem
+                  key={index}
+                  onClick={() => {
+                    handleClose(option)
+                  }}
+                  sx={{
+                    color: option?.type === 'delete' ? '#923534' : '#000000',
+                    '&:hover': {
+                      backgroundColor: 'transparent'
+                    }
+                  }}
+                >
+                  {option?.type === 'delete' ? <TrashDelete fill="#923534" width="30px" /> : option?.icon}{' '}
+                  {option?.label}
+                </MenuItem>
+                <Divider />
+              </>
+            ))}
+          </Menu>
+        </>
       )}
     </div>
   )
---
+++ src/components/IJ/learningCycleCard/components/menu/menuStyle.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const MenuIconOpen = styled.div`
+  width: 0;
+  height: 0;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-top: 10px solid black;
+  border-bottom: none;
+  border-top-width: 1px;
+  border-left-width: 1px;
+  border-right-width: 1px;
+`
