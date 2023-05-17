8ba4be70bdcb37c045d6b0146b84a5c564146294
Lucas de Souza <lucasdesouza@MacBook-Air-de-Lucas.local>
2023-05-17 14:15:55 -0300

    build

--- src/components/LXP/bannerLxp/bannerLxp.stories.tsx
+++ src/components/LXP/bannerLxp/bannerLxp.stories.tsx
@@ -28,9 +28,8 @@ bannerConfigs.args = {
   title: 'Nome do seu KnowHub',
   titleColor: '#FFF',
   isDisabledTitle: false,
-  // bgSrc:
-  //   'https://media.itpro.co.uk/image/upload/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1570815813/itpro/2018/01/shutterstock_712558591.jpg',
-  bgSrc: '',
+  bgSrc:
+    'https://media.itpro.co.uk/image/upload/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1570815813/itpro/2018/01/shutterstock_712558591.jpg',
   handleChangeBanner: (file) => console.log('imgBannerResp:', file),
   onSaveInfo: (nome) => alert(nome),
   bgColor: '',
