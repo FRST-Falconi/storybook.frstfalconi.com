109df428d03b77c2c3598671e5f72cb358d2efd0
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-05-22 13:50:13 -0300

    build

--- dist/src/shared/icons.d.ts
+++ dist/src/shared/icons.d.ts
@@ -151,6 +151,8 @@ export declare function IconGaps({ fill, width, height }: IconsProps): JSX.Eleme
 export declare function IconModules({ fill, width, height }: IconsProps): JSX.Element;
 export declare function CheckIconSimple({ width, height, fill }: IconsProps): JSX.Element;
 export declare function IconContent({ fill, width, height }: IconsProps): JSX.Element;
+export declare function IconLikeLine({ fill, stroke, customColor_1, width, height }: IconsProps): JSX.Element;
+export declare function IconLikeFilled({ fill, stroke, customColor_1, width, height }: IconsProps): JSX.Element;
 export declare function TrashIconNew({ fill, stroke, customColor_1, width, height }: IconsProps): JSX.Element;
 export {};
 //# sourceMappingURL=icons.d.ts.map
