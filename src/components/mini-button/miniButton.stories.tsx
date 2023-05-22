a1d2d288a07505777897190d43396f21ae0a4517
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-05-22 11:42:46 -0300

    feat: mini button component (#379)
    * feat: mini button component
    
    * build

---
+++ src/components/mini-button/miniButton.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Button from './index'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Buttons/Mini Button',
+    component: Button,
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />
+
+export const primary = Template.bind({})
+primary.args = {
+    variant: 'primary',
+    label: 'Button text',
+    disabled: false,
+    onClick: () => { alert('Click') }
+}
+
+
+export const secondary = Template.bind({})
+secondary.args = {
+    variant: 'secondary',
+    label: 'Button text',
+    disabled: false,
+    onClick: () => { alert('Click') }
+}
