775f83024e56add675ca844b5d032a36075a96c3
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-05-25 16:47:14 -0300

    feat: variant participant card

--- src/components/IJ/participantCardOld/index.tsx
+++ src/components/IJ/participantCardOld/index.tsx
@@ -10,6 +10,7 @@ import { AddPeople, MessageCheckLine } from '@shared/icons'
 import { useEffect, useState } from 'react'
 
 export default function ParticipantCardOld({
+  variant,
   userInfo,
   labels,
   successfullInvite,
@@ -39,7 +40,7 @@ export default function ParticipantCardOld({
 
   return (
     <ThemeProvider theme={FRSTTheme}>
-      <Styles.WrapperCard style={{ ...style }}>
+      <Styles.WrapperCard variant={variant} style={{ ...style }}>
         <Styles.UserInfo>
           <Avatar size="40px" src={userInfo?.avatar} />
           <Styles.DescriptionUser>
@@ -63,10 +64,10 @@ export default function ParticipantCardOld({
         <Styles.UserAdditionalInfo>
           {area && area?.length > 31 ? (
             <Tooltip position="top" textTooltip={userInfo?.area}>
-              <Styles.Area>{area}</Styles.Area>
+              <Styles.Area variant={variant}>{area}</Styles.Area>
             </Tooltip>
           ) : (
-            <Styles.Area>{area}</Styles.Area>
+            <Styles.Area variant={variant}>{area}</Styles.Area>
           )}
           {position && position?.length > 33 ? (
             <Tooltip position="top" textTooltip={userInfo?.position}>
@@ -76,33 +77,108 @@ export default function ParticipantCardOld({
             <Styles.Position>{position}</Styles.Position>
           )}
         </Styles.UserAdditionalInfo>
-
-        <Styles.FooterButton>
-          {statusSend == 'default' ? (
-            <>
-              {clickSendInvitation ? (
-                <Styles.ButtonSend onClick={() => clickSendInvitation()}>
-                  <AddPeople />
-                  {labels?.sendInvitation ? labels?.sendInvitation : 'Enviar convite'}
-                </Styles.ButtonSend>
-              ) : null}
-              {handleClickRemove ? (
-                <Styles.ButtonRemove onClick={() => handleClickRemove(userInfo?.id)}>
-                  {labels?.remove ? labels?.remove : 'Remover'}
-                </Styles.ButtonRemove>
-              ) : null}
-            </>
-          ) : null}
-          {statusSend == 'success' ? (
-            <div style={{ width: '100%', justifyContent: 'flex-start' }}>
-              <Styles.ButtonSuccess>
-                <MessageCheckLine width="14" />
-                {labels?.invitationSuccess ? labels?.invitationSuccess : 'Convite enviado'}
-              </Styles.ButtonSuccess>
-            </div>
-          ) : null}
-        </Styles.FooterButton>
+        {variant =='brainstorm' ? 
+            <FooterButtonBrainstorm
+              statusSend={statusSend}
+              clickSendInvitation={clickSendInvitation}
+              handleClickRemove={handleClickRemove}
+              labels={labels}
+              userInfo={userInfo}
+            />
+          :
+            <FooterButtonDefault            
+              statusSend={statusSend}
+              clickSendInvitation={clickSendInvitation}
+              handleClickRemove={handleClickRemove}
+              labels={labels}
+              userInfo={userInfo}
+            />
+        }
       </Styles.WrapperCard>
     </ThemeProvider>
+  )
+}
+
+
+function FooterButtonBrainstorm({statusSend, clickSendInvitation, handleClickRemove, labels, userInfo}) {
+  return (
+    <Styles.FooterButton style={{ justifyContent: 'flex-start', gap: '4px', alignItems: 'center'}}>
+      {statusSend == 'default' ? (
+        <>
+          {clickSendInvitation ? (
+            <Styles.ButtonSendV2 onClick={() => clickSendInvitation()}>
+              <div>
+                <LetterEnvelopLineIcon />
+              </div>
+              {labels?.sendInvitation ? labels?.sendInvitation : 'Convidar'}
+            </Styles.ButtonSendV2>
+          ) : null}
+          <Styles.ButtonOrV2>{labels.or}</Styles.ButtonOrV2>
+          {handleClickRemove ? (
+            <Styles.ButtonRemoveV2 onClick={() => handleClickRemove(userInfo?.id)}>
+              {labels?.remove ? labels?.remove : 'Remover'}
+            </Styles.ButtonRemoveV2>
+          ) : null}
+        </>
+      ) : null }
+      {statusSend == 'success' ? (
+        <div style={{ width: '100%', justifyContent: 'flex-start' }}>
+          <Styles.ButtonSuccessV2>
+            <CheckInCicle/>
+            {labels?.invitationSuccess ? labels?.invitationSuccess : 'Convite enviado'}
+          </Styles.ButtonSuccessV2>
+        </div>
+      ) : null}
+    </Styles.FooterButton>
+  )
+}
+
+function FooterButtonDefault({statusSend, clickSendInvitation, labels, handleClickRemove, userInfo}) {
+  return (
+    <Styles.FooterButton>
+      {statusSend == 'default' ? (
+        <>
+          {clickSendInvitation ? (
+            <Styles.ButtonSend onClick={() => clickSendInvitation()}>
+              <AddPeople />
+              {labels?.sendInvitation ? labels?.sendInvitation : 'Enviar convite'}
+            </Styles.ButtonSend>
+          ) : null}
+          {handleClickRemove ? (
+            <Styles.ButtonRemove onClick={() => handleClickRemove(userInfo?.id)}>
+              {labels?.remove ? labels?.remove : 'Remover'}
+            </Styles.ButtonRemove>
+          ) : null}
+        </>
+      ) : null}
+      {statusSend == 'success' ? (
+        <div style={{ width: '100%', justifyContent: 'flex-start' }}>
+          <Styles.ButtonSuccess>
+            <MessageCheckLine width="14" />
+            {labels?.invitationSuccess ? labels?.invitationSuccess : 'Convite enviado'}
+          </Styles.ButtonSuccess>
+        </div>
+      ) : null}
+    </Styles.FooterButton>
+  )
+}
+
+
+function LetterEnvelopLineIcon() {
+  return (
+    <div style={{height: '12px', borderRadius: '2px', overflow: 'hidden', marginRight: '8px'}} >
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12"  viewBox="1 1 16.4 12">
+        <path strokeWidth="1" fill='#0645ad' d="m3.2,0C1.43,0,0,1.43,0,3.2h2.4c0-.44.36-.8.8-.8V0Zm12,2.4c.44,0,.8.36.8.8h2.4c0-1.77-1.43-3.2-3.2-3.2v2.4Zm.8,8.8c0,.44-.36.8-.8.8v2.4c1.77,0,3.2-1.43,3.2-3.2h-2.4Zm-12.8.8c-.44,0-.8-.36-.8-.8H0c0,1.77,1.43,3.2,3.2,3.2v-2.4Zm-.8-.8V3.2H0v8h2.4Zm12.8.8H3.2v2.4h12v-2.4Zm.8-8.8v8h2.4V3.2h-2.4Zm-12.8-.8h12V0H3.2v2.4Zm4.48,4.44c.91.66,2.14.66,3.05,0l-.7-.97c-.49.35-1.15.35-1.64,0l-.7.97Zm3.05,0l6.43-4.65-.7-.97-6.43,4.65.7.97ZM1.25,2.19l6.43,4.65.7-.97L1.95,1.21l-.7.97Zm9.12,4.17l-.35-.49.35.49Zm-2.35,0l-.35.49.35-.49Z"/>
+    </svg>
+    </div>
+  )
+}
+
+function CheckInCicle() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="16" height="16" rx="8" fill="#1BA853"/>
+      <path d="M11.8806 5.09106L6.54729 10.4244L4.12305 8.00016" stroke="white" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
   )
 }
