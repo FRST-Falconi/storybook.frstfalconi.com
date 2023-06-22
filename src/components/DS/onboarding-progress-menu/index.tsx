c9d08631bd99d4de0484e50daae03d407a3b0bfe
Josiane Santos <josianesantos@frstfalconi.com>
2023-06-21 21:59:34 -0300

    feat: add props and the map of the step

--- src/components/DS/onboarding-progress-menu/index.tsx
+++ src/components/DS/onboarding-progress-menu/index.tsx
@@ -17,30 +17,41 @@ import {
     CheckCircle
 } from './onboardingProgressMenuStyles'
 import Avatar from '@components/avatar'
+import { Dot, Tick } from '@shared/icons'
+import { IOnboardingProgressMenu } from './onboardingProgressMenu'
 
-export default function OnboardingProgressMenu() {
+export default function OnboardingProgressMenu(props: IOnboardingProgressMenu) {
+    const { items, heading, level, challenge } = props
     return (
         <ThemeProvider theme={FRSTTheme}>
-            <Progress>Seu progresso</Progress>
+            <Progress>{heading}</Progress>
             <CardContainer>
                 <ImageTextGroup>
                     <Avatar size="55px" src="https://picsum.photos/200/300"></Avatar>
                     <div>
-                        <TextLevel>Iniciante</TextLevel>
-                        <Challenge>0 desafios</Challenge>
+                        <TextLevel>{level}</TextLevel>
+                        <Challenge>{challenge}</Challenge>
                     </div>
                 </ImageTextGroup>
                 <Line />
-                <StepBox>
-                    <StepBoxIconBlock>
-                        <CheckCircle></CheckCircle>
-                        <StepBar />
-                    </StepBoxIconBlock>
-                    <StepBoxTextBlock>
-                        <StepBoxTitle>Boas-Vindas</StepBoxTitle>
-                        <StepBoxDescription>Entenda a sua jornada contínua de aceleração de performance.</StepBoxDescription>
-                    </StepBoxTextBlock>
-                </StepBox>
+                {items.map((i, index) => (
+                    <StepBox>
+                        <StepBoxIconBlock>
+                            <CheckCircle isComplete={i.finished || i.isCurrent} isChecked={i.finished}>
+                                {i.finished ? <Tick width='12' /> :
+                                    <Dot width='8' height='8'/>}
+                            </CheckCircle>
+                            {
+                                items.length -1 > index &&
+                                <StepBar isComplete={i.finished} />
+                            }
+                        </StepBoxIconBlock>
+                        <StepBoxTextBlock>
+                            <StepBoxTitle href={i.titleUrl} isComplete={i.finished || i.isCurrent}>{i.title}</StepBoxTitle>
+                            <StepBoxDescription>{i.description}</StepBoxDescription>
+                        </StepBoxTextBlock>
+                    </StepBox>
+                ))}
             </CardContainer>
         </ThemeProvider>
     )
