f8af18cf0edd0757c81aa719c5ccbaff8435dbd8
inaralmeida <inara.ralmeida@gmail.com>
2023-06-28 19:12:44 -0300

    build

--- dist/src/components/LXP/extraContent/richTextEditor/components.d.ts
+++ dist/src/components/LXP/extraContent/richTextEditor/components.d.ts
@@ -3,19 +3,19 @@ interface BaseProps {
     className: string;
     [key: string]: unknown;
 }
-export declare const Button: React.ForwardRefExoticComponent<Omit<{
+export declare const Button: React.ForwardRefExoticComponent<Pick<{
     active: boolean;
     reversed: boolean;
-} & BaseProps, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export declare const EditorValue: React.ForwardRefExoticComponent<Omit<{
+} & BaseProps, keyof BaseProps> & React.RefAttributes<HTMLSpanElement>>;
+export declare const EditorValue: React.ForwardRefExoticComponent<Pick<{
     value: any;
-} & BaseProps, "ref"> & React.RefAttributes<null>>;
-export declare const Icon: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export declare const Instruction: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
-export declare const Menu: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
+} & BaseProps, keyof BaseProps> & React.RefAttributes<null>>;
+export declare const Icon: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLSpanElement>>;
+export declare const Instruction: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
+export declare const Menu: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
 export declare const Portal: ({ children }: {
     children: any;
 }) => any;
-export declare const Toolbar: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
+export declare const Toolbar: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
 export {};
 //# sourceMappingURL=components.d.ts.map
