6c039ba3ebaade4313f0cb6da3f27335a4ec6c8d
Josiane Santos <127220570+josianesantos@users.noreply.github.com>
2023-06-28 15:50:20 -0300

    feat: add shadow in CardContainer (#437)
    * feat: add shadow in CardContainer
    
    * feat: add build

--- dist/src/components/LXP/extraContent/richTextEditor/components.d.ts
+++ dist/src/components/LXP/extraContent/richTextEditor/components.d.ts
@@ -3,19 +3,19 @@ interface BaseProps {
     className: string;
     [key: string]: unknown;
 }
-export declare const Button: React.ForwardRefExoticComponent<Pick<{
+export declare const Button: React.ForwardRefExoticComponent<Omit<{
     active: boolean;
     reversed: boolean;
-} & BaseProps, keyof BaseProps> & React.RefAttributes<HTMLSpanElement>>;
-export declare const EditorValue: React.ForwardRefExoticComponent<Pick<{
+} & BaseProps, "ref"> & React.RefAttributes<HTMLSpanElement>>;
+export declare const EditorValue: React.ForwardRefExoticComponent<Omit<{
     value: any;
-} & BaseProps, keyof BaseProps> & React.RefAttributes<null>>;
-export declare const Icon: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLSpanElement>>;
-export declare const Instruction: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
-export declare const Menu: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
+} & BaseProps, "ref"> & React.RefAttributes<null>>;
+export declare const Icon: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLSpanElement>>;
+export declare const Instruction: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
+export declare const Menu: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
 export declare const Portal: ({ children }: {
     children: any;
 }) => any;
-export declare const Toolbar: React.ForwardRefExoticComponent<Pick<BaseProps, keyof BaseProps> & React.RefAttributes<HTMLDivElement>>;
+export declare const Toolbar: React.ForwardRefExoticComponent<Omit<BaseProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
 export {};
 //# sourceMappingURL=components.d.ts.map
