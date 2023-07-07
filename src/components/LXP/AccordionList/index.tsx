686ec383e82aa2d336f367b23ee9fc12bb698146
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2023-07-07 12:54:13 -0300

    feat: update component

--- src/components/LXP/AccordionList/index.tsx
+++ src/components/LXP/AccordionList/index.tsx
@@ -1,5 +1,5 @@
 import { ArrowScrollLeft } from '@shared/icons'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FRSTTheme } from '../../../theme'
 
 import * as S from './AccordionList.styles'
@@ -7,20 +7,36 @@ import * as S from './AccordionList.styles'
 type TAccordionListprops = {
   title?: string
   children?: React.Component
-  isOpen: boolean
+  isOpen?: boolean
   setIsOpen: any
+  selectedItem: boolean
 }
 
-export default function AccordionList({ title = 'Módulo', children, isOpen = false, setIsOpen }: TAccordionListprops) {
+export default function AccordionList({
+  title = 'Módulo',
+  children,
+  isOpen = false,
+  setIsOpen,
+  selectedItem
+}: TAccordionListprops) {
   const [open, setOpen] = useState(isOpen)
+  const [selected, setSelected] = useState(selectedItem)
 
   const handleSetOpen = () => {
     setOpen((prev) => !prev)
     setIsOpen()
   }
+
+  useEffect(() => {
+    if (selectedItem) {
+      setOpen(true)
+    }
+    setSelected(selectedItem)
+  }, [selectedItem])
+
   return (
-    <S.StylesAccordionList theme={FRSTTheme} className={open ? 'open' : 'closed'}>
-      <div onClick={handleSetOpen} className={open ? 'header open' : 'header closed'}>
+    <S.StylesAccordionList theme={FRSTTheme} selectedItem={selected} className={selected ? 'open' : 'closed'}>
+      <div onClick={handleSetOpen} className={selected ? 'header open' : 'header closed'}>
         <p className="title">{title}</p>
         <span className={open ? 'open' : 'closed'}>
           <ArrowScrollLeft height="16" width="16" />
