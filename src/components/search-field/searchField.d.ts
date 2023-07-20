9bed86bd840cc4579e30da99a2a63e91020bc870
bgfenelon <biafenelon2014@gmail.com>
2023-07-20 13:44:20 -0300

    ajuste search field

--- src/components/search-field/searchField.d.ts
+++ src/components/search-field/searchField.d.ts
@@ -7,7 +7,7 @@ export interface ISearchField {
   value?: string
   onChange?: (e) => void
   textButton?: string
-  handleClickButton: () => void
+  handleClickButton: (e) => void
   className?: string
   handleKeyPress?: (e) => void
   isButton?: boolean
