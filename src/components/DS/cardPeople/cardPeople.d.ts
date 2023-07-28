34a975dea7bd1c1b81d8e3bd215f99c6f9b53029
Josiane Santos <josianesantos@frstfalconi.com>
2023-07-28 00:18:29 -0300

    feat :  CardPeople adjustments

--- src/components/DS/cardPeople/cardPeople.d.ts
+++ src/components/DS/cardPeople/cardPeople.d.ts
@@ -1,19 +1,38 @@
-
+import React from 'react'
 export interface ICardPeople {
     userInfo: {
+        id: string
+        name: string
+        avatar: string
+        email: string
+        area: string
+        position: string
+    }
+    labels: {
+        area: string
+        position: string
+    },
+    hiddenCheckbox?: boolean
+    isChecked?: boolean
+    onChecked: (data: OnCheckedParams) => void
+    style?: React.CSSProperties
+}
+
+export interface IPositionContainer {
+    hiddenCheckbox: boolean
+} 
+
+export interface IPeopleContainer {
+    isChecked?: boolean
+}
+
+export interface OnCheckedParams {
+        isChecked?: boolean
         id: string,
         name: string,
         avatar: string,
         email: string,
         area: string,
         position: string,
-    }
-    labels?: {
-        area: string,
-        position: string
-    },
 }
 
-export interface IPeopleContainer {
-    isChecked: boolean
-}
--- src/components/DS/cardPeople/cardPeople.stories.tsx
+++ src/components/DS/cardPeople/cardPeople.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import CardPeople from './index'
 
 export default {
@@ -11,11 +11,12 @@ const Template = (args) => <CardPeople {...args} />
 export const normal =  Template.bind({})
 normal.args = {
     isChecked: false,
+    style:{width:'290px'},
     userInfo: {
-        id: '',
-        name: '',
-        avatar: '',
-        email: '',
+        id: 'd914dacb-d633-4744-9b0a-2b19c2e3f231',
+        name: 'Bernardo Silva',
+        avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+        email: 'nome@frstfalconi.com',
         area: 'Tecnologia da informação',
         position: 'Gerente',
     },
@@ -23,4 +24,24 @@ normal.args = {
         area: 'Área',
         position: 'Cargo'
     },
+    onChecked: (e) => console.log('onChecked', e),
+    hiddenCheckbox: false
+}
+
+export const noCheckBox = Template.bind({})
+noCheckBox.args = {
+    style:{width:'290px'},
+    userInfo: {
+        id: 'd914dacb-d633-4744-9b0a-2b19c2e3f231',
+        name: 'Bernardo Silva',
+        avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+        email: 'nome@frstfalconi.com',
+        area: 'Tecnologia da informação',
+        position: 'Gerente',
+    },
+    labels: {
+        area: 'Área',
+        position: 'Cargo'
+    },
+    hiddenCheckbox: true
 }
