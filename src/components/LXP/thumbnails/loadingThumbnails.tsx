7b6caf5a6ca9f9b4de169cd4493ecc5722c27115
inaralmeida <inara.ralmeida@gmail.com>
2023-07-31 19:05:16 -0300

    feat: loading thumbnail

---
+++ src/components/LXP/thumbnails/loadingThumbnails.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as Styles from './thumbnailsStyle'
+import HeaderVectorElipses from './headerVectorElipses'
+export const LoadingThumbnails = ({ provided }) => {
+  return (
+    <Styles.CardDragAndDrop className="loading">
+      <HeaderVectorElipses provided={provided} />
+      <Styles.LoadingContainer className="loading_img"></Styles.LoadingContainer>
+      <Styles.LoadingContainer className="loading_text"></Styles.LoadingContainer>
+      <Styles.LoadingContainer className="loading_text loading_button"></Styles.LoadingContainer>
+    </Styles.CardDragAndDrop>
+  )
+}
