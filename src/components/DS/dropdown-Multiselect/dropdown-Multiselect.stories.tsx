c642c16742e827a1829dcde9dfd4f4dcf46e415f
Marlon <marlonps94@gmail.com>
2023-08-02 11:32:59 -0300

    Feat/dropdown multiselect (#467)
    * novo componente de multiselect
    
    * build

---
+++ src/components/DS/dropdown-Multiselect/dropdown-Multiselect.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import DropdownMultiselect from './index'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'DS/DropdownMultiselect',
+  component: DropdownMultiselect,
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <DropdownMultiselect {...args} />
+
+export const normal = Template.bind({})
+normal.args = {
+    maxSelectedShow: 5,
+    selectPlaceholder: 'Selecione aqui',
+    searchSelectPlaceholder: 'Pesquisar',
+    isDisabled: false,
+    removeItemsToolTip: 'Excluir todos',
+    style: {width: '400px'},
+    modalTitle: 'Este grupo é administrado por',
+    btnSelectAllText: 'Selecionar todos',
+    removeModalText: 'Remover',
+    getSelectedItems: (item) => console.log("Selecionado: ", item),
+    listItems: [
+        { id: 1, name: 'Australia', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 2, name: 'Brazil', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 3, name: 'China', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 4, name: 'Egypt', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 5, name: 'France', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 6, name: 'Germany', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 7, name: 'India', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 8, name: 'Japan', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 9, name: 'Spain', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 10, name: 'United States', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 11, name: 'Au', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 12, name: 'Brazil', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 13, name: 'China', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 14, name: 'Egypt', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 15, name: 'France', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 16, name: 'Germany', description: 'FRST Falcon', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 17, name: 'India', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 18, name: 'Japan', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 19, name: 'Spain', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' },
+        { id: 20, name: 'United States', description: 'FRST Falconi', avatar: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg' }
+    ]
+}
---
+++ src/components/DS/dropdown-Multiselect/styles/multiselectStyles.ts
@@ -0,0 +1,222 @@
+import styled from "styled-components";
+
+export const containerSelect = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: row-reverse;
+    gap: 12px;
+    width: 600px;
+    min-height: 48px;
+    position: relative;
+
+    & > p {
+        font-family: 'PT Sans';
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 20.7px;
+        color: ${(props) => props.theme.colors.neutralsGrey1};
+    }
+`
+
+export const headerSelect = styled.div`
+    display: inline-flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
+    cursor: pointer;
+    overflow: hidden;
+    /* position: absolute; */
+    /* z-index: 999; */
+    background: ${(props) => props.theme.colors.shadeWhite};
+    border: 1px solid ${(props) => props.theme.colors.borderPrimary};
+    border-radius: 8px;
+    min-height: 48px;
+    display: flex;
+    width: 100%;
+    padding: 8px 52px 8px 10px;
+`
+
+export const customSelect = styled.div`
+    & .custom-multiselect {
+        background: transparent;
+        /* background: ${(props) => props.theme.colors.shadeWhite}; */
+        /* border: 1px solid ${(props) => props.theme.colors.borderPrimary}; */
+        border-radius: 8px;
+        height: 100%;
+        display: flex;
+        width: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+
+        :hover {
+            border: 1px solid #F18624;
+        }
+
+        .p-placeholder {
+            color: ${(props) => props.theme.colors.neutralsGrey4};
+            font-weight: 400;
+            font-size: 14px;
+            font-family: 'PT Sans';
+            line-height: 18.12px;
+        }
+
+        .p-multiselect-items {
+            display: flex;
+            width: 100%;
+        }
+
+        .p-multiselect-label-container {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+        
+    }
+    .p-multiselect:not(.p-disabled).p-focus {
+        box-shadow: none;
+        border: 2px #f26818 solid;
+    }
+
+`
+
+export const searchAndButton = styled.div`
+    display: flex;
+    padding: 1rem;
+    flex-direction: column;
+`
+
+export const selectItem = styled.div`
+    display: inline-flex;
+    align-items: center;
+    gap: 8px;
+    font-family: 'PT Sans';
+    font-size: 16px;
+    font-weight: 400;
+    color: ${ props => props.theme.colors.neutralsGrey1};
+
+`
+
+export const selectTag = styled.div`
+    display: inline-flex;
+    align-items: center;
+    height: 32px;
+    background: #00828C;
+    border-radius: 4px;
+    gap: 8px;
+    padding: 4px 8px;
+    margin: 5px 10px 5px 0;
+    z-index: 999;
+
+    :hover {
+        background: #1F6E74;
+    }
+
+    & > p {
+        font-family: 'PT Sans';
+        font-size: 14px;
+        font-weight: 700;
+        line-height: 18.12px;
+        text-align: center;
+        color: ${ props => props.theme.colors.shadeWhite };
+    }
+`
+
+export const overShowInfo = styled.div`
+    height: 32px;
+    padding: 4px 8px;
+    margin: 5px 10px 5px 0;
+    z-index: 999;
+    cursor: pointer;
+
+    & :hover{
+        color: ${ props => props.theme.colors.neutralsGrey1 };
+    }
+
+    & > p {
+        font-family: 'PT Sans';
+        font-size: 14px;
+        font-weight: 700;
+        line-height: 18.12px;
+        text-align: center;
+        color: ${ props => props.theme.colors.neutralsGrey2 };
+    }
+`
+
+export const modalContainer = styled.div`
+    width: 520px;
+    max-height: 500px;
+
+    display: flex;
+    flex-direction: column;
+    background: ${props => props.theme.colors.shadeWhite};
+    border: 2px solid ${props => props.theme.colors.borderPrimary};
+    border-radius: 16px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    overflow: hidden;
+`
+
+export const modalHeader = styled.div`
+    padding: 36px 24px;
+    & > p {
+        font-family: 'Work Sans';
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 21.11px;
+        color: ${props => props.theme.colors.neutralsGrey1};
+    }
+`
+
+export const modalContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-track {
+        display: none;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        width: 4px;
+        border-radius: 12px;
+        background: #BDBDBD;
+    }
+`
+
+export const modalCards = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    padding: 24px;
+`
+
+export const cardTitle = styled.div`
+    font-family: 'PT Sans';
+    font-size: 16px;
+    line-height: 17.6px;
+    font-weight: 700;
+    color: ${props => props.theme.colors.neutralsGrey1};;
+`
+
+export const cardDescription = styled.div`
+    font-family: 'PT Sans';
+    font-size: 16px;
+    line-height: 17.6px;
+    font-weight: 400;
+    color: ${props => props.theme.colors.neutralsGrey1};;
+`
+
+export const closeModal = styled.div`
+    position: absolute;
+    top: 12px;
+    right: 20px;
+`
