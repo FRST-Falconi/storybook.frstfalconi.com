269d9a7f9e3f807e0a6fefa16288bad3f80070dc
Josiane Santos <127220570+josianesantos@users.noreply.github.com>
2023-08-03 14:22:57 -0300

    feat: add variant de adm button in table (#470)
    * feat: add variant de adm button in table
    
    * build

--- src/components/DS/groups-table/index.tsx
+++ src/components/DS/groups-table/index.tsx
@@ -198,11 +198,20 @@ export default function GroupsTable({
 													boxShadow:' 0px 0px 18px 0px rgba(34, 34, 34, 0.2)'
 												}}
 											>
+												{i.adms.length < 9 ? 	
 												<AdmButton
 													onClick={() => onShowMoreClick(i.id)}
 													variant={'count'}
 													count={i.adms.length - maxAdmToShow}
+												/> 
+												:
+												<AdmButton
+													onClick={() => onShowMoreClick(i.id)}
+													variant={'countMore'}
+													count={9}
 												/>
+												}
+											
 											</Tooltip>
 
 
