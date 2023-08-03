e315fe7b633569d886289cfe66fb9bdad37225d6
Josiane Santos <josianesantos@frstfalconi.com>
2023-08-03 13:31:01 -0300

    feat: add variant de adm button in table

--- src/components/DS/admButton/index.tsx
+++ src/components/DS/admButton/index.tsx
@@ -3,13 +3,21 @@ import { FRSTTheme } from '../../../theme'
 import { AvatarContent, PlusButton, ImageAvatarContent, ImageContent } from './admButton.styles'
 import { EmptyAvatar } from '@shared/icons'
 import { IAdmButton } from './admButton'
+import { useMemo } from 'react'
 
 export default function AdmButton({ variant, count, image, onClick }: IAdmButton) {
+
+    const backgroudVariant = useMemo(() =>{
+        if (variant === 'count' || variant === 'countMore') return'rgba(68, 68, 68, 1)'
+        return '#E0E0E0'
+        
+    }, [variant] )
+
     return (
         <ThemeProvider theme={FRSTTheme}>
-                <AvatarContent onClick={onClick} background={variant != 'count' ? '#E0E0E0' :
-                    'rgba(68, 68, 68, 1)'}>
+                <AvatarContent onClick={onClick} background={backgroudVariant}>
                     {variant == 'count' && <div style={{paddingRight: '6px'}}>+{count}</div>}
+                    {variant == 'countMore' && <div style={{paddingRight: '6px'}}>{count}+</div>}
                     {variant == 'add' && (
                         <>
                             <ImageAvatarContent>
