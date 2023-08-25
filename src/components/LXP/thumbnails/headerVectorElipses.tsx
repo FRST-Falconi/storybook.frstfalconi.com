6bda7e79b158f404b7b1ac57f889abd102dff99c
Inara Almeida <inara.ralmeida@gmail.com>
2023-08-25 17:05:48 -0300

    fix: header elipses component

--- src/components/LXP/thumbnails/headerVectorElipses.tsx
+++ src/components/LXP/thumbnails/headerVectorElipses.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 import styled from 'styled-components'
 import VectorEllipse from './vectorEllipse'
 
-const HeaderVectorElipses = ({ provided }) => {
+
+
+const HeaderVectorElipses = ({ provided, direction }) => {
   return (
-    <div ref={provided ? provided.innerRef : null} {...(provided ? provided.dragHandleProps : null)}>
+    <div ref={provided ? provided.innerRef : null} {...(provided ? provided.dragHandleProps : null)} style={{display: 'flex', width:'fit-content', height:'fit-content', flexDirection: direction ? 'row' : 'column'}}>
       {[1, 2, 3].map((thumb) => {
         const arr = new Array(10).fill('')
         return (
-          <div style={{ display: 'flex', justifyContent: 'center', cursor: 'pointer' }} key={thumb}>
+          <div style={{ width: 'fit-content', display: 'flex', justifyContent: 'center', cursor: 'pointer', flexDirection: !direction ? 'row' : 'column'}} key={thumb}>
             {arr.map((i, k) => {
               return <VectorEllipse key={k} />
             })}
--- src/components/LXP/thumbnails/loadingThumbnails.tsx
+++ src/components/LXP/thumbnails/loadingThumbnails.tsx
@@ -4,7 +4,7 @@ import HeaderVectorElipses from './headerVectorElipses'
 export const LoadingThumbnails = ({ provided }) => {
   return (
     <Styles.CardDragAndDrop className="loading">
-      <HeaderVectorElipses provided={provided} />
+      <HeaderVectorElipses provided={provided} direction='row'/>
       <Styles.LoadingContainer className="loading_img"></Styles.LoadingContainer>
       <Styles.LoadingContainer className="loading_text"></Styles.LoadingContainer>
       <Styles.LoadingContainer className="loading_text loading_button"></Styles.LoadingContainer>
