39b50cd49c1bfd4f52edc720dc4f27d1d22feaeb
edilsonandrade <edilson.a.andrade@gmail.com>
2023-12-05 19:01:24 -0300

    feat: on select an user change color and send to the client

--- src/components/mentions/index.tsx
+++ src/components/mentions/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {
   Container,
   MentionAvatar,
@@ -9,7 +10,7 @@ import {
   MentionUserName
 } from './mentionStyle'
 
-interface User {
+export interface User {
   id: number
   name: string
   avatar: string
@@ -19,18 +20,28 @@ export interface MentionProps
   extends Readonly<{
     users: User[]
     height: number,
-    width: number
-
+    width: number,
+    onSelect: (user: User) => void
+    inputSearch?: string
   }> {}
 
 export const Mentions = (mention: MentionProps) => {
+  const {onSelect, users, inputSearch} = mention;
+  const [selectedUser, setSelectedUser] = useState<number | null>()
+
+  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(inputSearch?.replace('@', '')?.toLowerCase() || ''))
   return (
     <>
       <Container>
       <MentionList>
-        {mention?.users?.map((user) => {
+        {filteredUsers?.map((user) => {
           return (
-            <MentionItem key={user.id}>
+            <MentionItem 
+              key={user.id} 
+              active={user.id === selectedUser} 
+              onMouseDown={()=>setSelectedUser(user.id)} 
+              onClick={()=> onSelect(user)}
+            >
               <MentionAvatar src={user.avatar} />
               <MentionUserContainer>
                 <MentionUserName>{user.name}</MentionUserName>
