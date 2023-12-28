f741a06e52817a2dab20e13cfc80d49c3c9122dc
Italo Costa <costa.italo.ti@gmail.com>
2023-12-28 13:52:17 -0300

    feat/update dist

--- src/components/FI/ThreadComments/threadComments.stories.tsx
+++ src/components/FI/ThreadComments/threadComments.stories.tsx
@@ -13,15 +13,19 @@ export default {
   
 export const Default = Template.bind({})
 
+
 const listReplyComments = [{
     id:"02",
     text: "Disse tudo Leticia! https://frstfalconi.com/ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andUow, muito genial isso https://frstfalconi.com/ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
     howLongAgo: "2 horas atrás",
     user:{
-        uuid: "user-00001",
+        user_uuid: "user-00001",
         name: "Julio Neto",
-        role_name:"Gestor de Trafego",
-        company_name: "Frst Falconi",
+        profile:{
+            role_name:"Gestor de Trafego",
+            company_name: "Frst Falconi",
+            avatar: "",
+        }
     },
 },
 {
@@ -30,10 +34,13 @@ const listReplyComments = [{
     howLongAgo: "1 horas atrás",
     mentionText: "<div><p>Este é um comentario para o usuario <span style='color: rgb(242, 104, 24);'>Joao</span> que vai ficar marcado no mentions https://frstfalconi.com/ </p></div>",
     user:{
-        uuid: "user-00002",
+        user_uuid: "user-00002",
         name: "Lucas Silva",
-        role_name:"Gestor De Finanças",
-        company_name: "Frst Falconi",
+        profile:{
+            role_name:"Gestor De Finanças",
+            company_name: "Frst Falconi",
+            avatar: ""
+        }
     },
 }
 ]
@@ -49,9 +56,11 @@ Default.args = {
         user:{
             uuid: "user-0001",
             name: "Leticia Costa",
-            role_name:"Líder de vendas",
-            company_name: "Frst Falconi",
-            avatar: "https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg"
+            profile:{
+                role_name:"Líder de vendas",
+                company_name: "Frst Falconi",
+                avatar: "https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg"
+            }
         },
         id:"01",
         text: "Uow, que projeto incrível, ansioso para ver os proximos passos  https://frstfalconi.com/",
