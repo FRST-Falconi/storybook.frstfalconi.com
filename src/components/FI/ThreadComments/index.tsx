5e65b5c1d65d590ec942922f9f9a26b719de0104
Italo Costa <costa.italo.ti@gmail.com>
2024-01-16 13:57:02 -0300

    new Teste

--- src/components/FI/ThreadComments/index.tsx
+++ src/components/FI/ThreadComments/index.tsx
@@ -112,7 +112,7 @@ export const ThreadComments = ({
               <Fragment key={replyComment.id}>
                 <CommentaryBoxReply
                   commentData={replyComment}
-                  answerButtonText={''}
+                  answerButtonText={'Responder'}
                   showMoreButtonText={showMoreButtonText}
                   showLessButtonText={showLessButtonText}
                   onClickAnswerButton={handleCommentReplyReply}
