5a58330810bc872018fa5f109b717d7baa274739
Italo Costa <costa.italo.ti@gmail.com>
2024-01-24 13:23:46 -0300

    update MenuMore for commentaryBoxv2

--- src/components/menu-more/menuMore.d.ts
+++ src/components/menu-more/menuMore.d.ts
@@ -4,13 +4,14 @@ import React from 'react'
 export interface IOptionItem {
     description: string
     startIcon: any
-    onClick: (e?:any) => void
-    color?:  string
+    onClick: (e?: any) => void
+    color?: string
 }
 
 export interface IMenuMore {
     iconButton?: any
-    options:  Array<IOptionItem>
+    options: Array<IOptionItem>
     style?: React.CSSProperties;
     closeAfterClick?: boolean;
+    isHover?: boolean;
 }
