ac28cc2cb4b7480846ad07599020cebf7217678d
Italo Costa <costa.italo.ti@gmail.com>
2024-01-29 08:53:41 -0300

    update dist

--- dist/src/components/search-field/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import '../../shared/global.css';
-import { ISearchField } from './searchField';
-export default function SearchField({ label, hasSearchIcon, placeholder, value, onChange, textButton, className, handleClickButton, isButton, id }: ISearchField): JSX.Element;
-//# sourceMappingURL=index.d.ts.map
