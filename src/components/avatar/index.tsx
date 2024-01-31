0351e928e49ca40991de192fc160f7790ac2c07c
@Guilvitor17 <109220774+GuilVitor17@users.noreply.github.com>
2024-01-31 17:04:26 -0300

    Feat/ajuste avatar (#585)
    * ajustando tela de detalhes de desafio para telas menores, e trocando url de avatar
    
    * build

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -5,7 +5,8 @@ import * as Styles from './avatarStyles'
 import { IAvatar } from './avatar'
 
 export default function Avatar({ size, src, alt, className, disabled, onClick, isActiveClick, id, style }: IAvatar) {
-  const defaultImg = 'https://i.gyazo.com/499dda909b1ebfe0375d1efa2d5d00a8.png'
+  const defaultImg = 'https://cdn-images.frstfalconi.cloud/Avatar_default.png'
+  
   return (
     <ThemeProvider theme={FRSTTheme}>
       <Styles.AvatarWrapper size={size} className={className} onClick={onClick} isActiveClick={isActiveClick} id={id} style={style}>
