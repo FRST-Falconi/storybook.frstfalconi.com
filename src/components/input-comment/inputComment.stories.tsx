e3baea105982ab47cff8b340d3d768ef681823ab
Italo Costa <costa.italo.ti@gmail.com>
2024-02-01 17:43:14 -0300

    remove comment stories input comment

--- src/components/input-comment/inputComment.stories.tsx
+++ src/components/input-comment/inputComment.stories.tsx
@@ -34,32 +34,32 @@ normal.args = {
   styles: { margin: '0 0 0 0' },
   emojiWindowlanguage: 'pt',
   users: mentionUserList,
-  //   replyMentionedUser: {
-  //     user_uuid: '8735edca-e00c-11ec-8e40-0a3879a705c9',
-  //     user_email: 'abadia.ribeiro@eletronorte.gov.br',
-  //     slug: 'abadiaribeiroeletronortegovbr',
-  //     name: 'Edilson Augusto de Andrade',
-  //     profile: {
-  //       uuid: '8735edca-e00c-11ec-8e40-0a3879a705c9',
-  //       first_name: 'Edilson',
-  //       last_name: 'Edilson Ribeiro De Souza',
-  //       email: 'abadia.ribeiro@eletronorte.gov.br',
-  //       phone: '',
-  //       language: 'pt-br',
-  //       avatar: '',
-  //       profession: '',
-  //       linkedin: '',
-  //       theme_color: '',
-  //       theme_type: '',
-  //       bio: '',
-  //       area_uuid: '78da5040-e393-4ae7-b6f7-c8aafbe045a0',
-  //       area_name: 'Outro',
-  //       role_uuid: '1aa1ef3b-c9d1-4bf6-af34-d531f312fb07',
-  //       role_name: 'Outro(a)',
-  //       company_uuid: 'a61db098-0956-458c-9306-db8d3ccfc889',
-  //       company_name: 'Centrais Eletricas Brasileiras SA | Eletrobras'
-  //     }
-  //   },
+  replyMentionedUser: {
+    user_uuid: '8735edca-e00c-11ec-8e40-0a3879a705c9',
+    user_email: 'abadia.ribeiro@eletronorte.gov.br',
+    slug: 'abadiaribeiroeletronortegovbr',
+    name: 'Edilson Augusto de Andrade',
+    profile: {
+      uuid: '8735edca-e00c-11ec-8e40-0a3879a705c9',
+      first_name: 'Edilson',
+      last_name: 'Edilson Ribeiro De Souza',
+      email: 'abadia.ribeiro@eletronorte.gov.br',
+      phone: '',
+      language: 'pt-br',
+      avatar: '',
+      profession: '',
+      linkedin: '',
+      theme_color: '',
+      theme_type: '',
+      bio: '',
+      area_uuid: '78da5040-e393-4ae7-b6f7-c8aafbe045a0',
+      area_name: 'Outro',
+      role_uuid: '1aa1ef3b-c9d1-4bf6-af34-d531f312fb07',
+      role_name: 'Outro(a)',
+      company_uuid: 'a61db098-0956-458c-9306-db8d3ccfc889',
+      company_name: 'Centrais Eletricas Brasileiras SA | Eletrobras'
+    }
+  },
   group_uuid: 'b1005836-b0a6-4a50-8147-537ebdc64a75',
   limitMessageExceeded: 'Limite de caracteres excedido'
 }
