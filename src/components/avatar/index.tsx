6494c688c9f60bf866317bd289431d09f8dd7da3
Guil Vitor <guilvitor17@gmail.com>
2024-02-06 12:13:32 -0300

    resolvendo bug de avatar quebrado quando não tem imagem

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -3,14 +3,21 @@ import { ThemeProvider } from 'styled-components'
 import { FRSTTheme } from '../../theme'
 import * as Styles from './avatarStyles'
 import { IAvatar } from './avatar'
+import { useEffect, useState } from 'react'
 
 export default function Avatar({ size, src, alt, className, disabled, onClick, isActiveClick, id, style }: IAvatar) {
+  const [isImage, setIsImage] = useState(false)
+
   const defaultImg = 'https://cdn-images.frstfalconi.cloud/Avatar_default.png'
-  
+
+  useEffect(() => {
+    setIsImage(!!src);
+  }, [src]);
+
   return (
     <ThemeProvider theme={FRSTTheme}>
       <Styles.AvatarWrapper size={size} className={className} onClick={onClick} isActiveClick={isActiveClick} id={id} style={style}>
-        <Styles.AvatarImg src={src || defaultImg} alt={alt} size={size} disabled={disabled} />
+        <Styles.AvatarImg src={isImage ? src : defaultImg} alt={alt} size={size} disabled={disabled}/>
       </Styles.AvatarWrapper>
     </ThemeProvider>
   )
