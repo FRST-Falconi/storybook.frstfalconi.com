074c7c70e2b2c7c881890ea92d54104263306cce
Guil Vitor <guilvitor17@gmail.com>
2024-03-06 10:18:22 -0300

    remove alt and default

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -6,18 +6,11 @@ import { IAvatar } from './avatar'
 import { useEffect, useState } from 'react'
 
 export default function Avatar({ size, src, alt, className, disabled, onClick, isActiveClick, id, style }: IAvatar) {
-  const [isImage, setIsImage] = useState(false)
-
-  const defaultImg = 'https://cdn-images.frstfalconi.cloud/Avatar_default.png'
-
-  useEffect(() => {
-    setIsImage(!!src);
-  }, [src]);
 
   return (
     <ThemeProvider theme={FRSTTheme}>
       <Styles.AvatarWrapper size={size} className={className} onClick={onClick} isActiveClick={isActiveClick} id={id} style={style}>
-        <Styles.AvatarImg src={isImage ? src : defaultImg} size={size} disabled={disabled}/>
+        <Styles.AvatarImg src={ src }  size={size} disabled={disabled}/>
       </Styles.AvatarWrapper>
     </ThemeProvider>
   )
