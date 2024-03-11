ab842ba829acb7f391140e0ef04e71466e83f042
Bianca Gomes <117212676+biaGomesFrst@users.noreply.github.com>
2024-03-11 14:15:01 -0300

    ajustes quebra de texto (#620)
    * ajustes quebra de texto
    
    * ajustando BoxV2
    
    ---------
    
    Co-authored-by: Bianca Gomes <biafenelon2014@gmail.com>

--- src/components/commentaryBoxV2/CommentaryBoxV2.stories.tsx
+++ src/components/commentaryBoxV2/CommentaryBoxV2.stories.tsx
@@ -15,8 +15,7 @@ Default.args = {
   userOffice: 'Líder de vendas',
   userCompany: 'Frst Falconi',
   imgProfile: 'https://i.pinimg.com/474x/2b/27/2f/2b272f528c3e14054436edd0d8bd2a26.jpg',
-  commentTextWithMention:
-    '<p>Marcando <a data-mention-id="0ceac4cb-7bc4-4ac0-a661-35cc7dfe4985" contenteditable="false" href="/profile/0ceac4cb-7bc4-4ac0-a661-35cc7dfe4985" style="font-weight: bold; color: rgb(242, 104, 24);">Alexandre Correa Da Silva</a>&nbsp;</p>',
+  commentTextWithMention:'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkllllllllllllllkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll',
   commentId: 'commentId-0001',
   commentText:
     "Uow, muito genial isso https://frstfalconi.com/  Uow, muito genial isso https://frstfalconi.com/ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andUow, muito genial isso https://frstfalconi.com/ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and",
