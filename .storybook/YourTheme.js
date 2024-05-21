ff098573f6ec23db165cd34ce6f99e10a222f05f
Josiane Santos <josianesantos@frstfalconi.com>
2024-05-21 13:52:50 -0300

    feat: add  dark theme in storybook

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'My custom Storybook',
+    brandImage: 'https://frstfalconi.com/wp-content/uploads/2024/02/Illustration-3.svg',
+    brandTarget: '_self',
+});
