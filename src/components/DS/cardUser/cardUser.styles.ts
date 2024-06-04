94943c6bcc665d3a53b6de0c8597169a2872d72f
Josiane Santos <josianesantos@frstfalconi.com>
2024-06-04 16:04:40 -0300

    feat: add variante de botao no componente de card de usuarios

--- src/components/DS/cardUser/cardUser.styles.ts
+++ src/components/DS/cardUser/cardUser.styles.ts
@@ -1,9 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const UserContainer = styled.div`
+interface ContainerProps{
+    variant: boolean
+}
+
+export const UserContainer = styled.div<ContainerProps>`
     width: 100%;
     display: flex;
-    min-height: 208px;
     position: relative;
     flex-direction:column;
     align-items:center;
@@ -11,10 +14,22 @@ export const UserContainer = styled.div`
     background:${({ theme }) => theme.colors.shadeWhite};
     border-radius: 12px;
     box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
-    padding-top: 18px;
-    padding-left:16px;
-    padding-right: 16px;
-    padding-bottom: 22px;
+
+    ${
+        (props) => props.variant ? css`
+            padding-top: 12px;
+            padding-left:16px;
+            padding-right: 16px;
+            padding-bottom: 16px;
+            min-height: 231px;
+        ` : css`
+            padding-top: 18px;
+            padding-left:16px;
+            padding-right: 16px;
+            padding-bottom: 22px;
+            min-height: 208px;
+        `
+    }
 `
 
 export const Headers = styled.div`
@@ -23,6 +38,21 @@ export const Headers = styled.div`
         flex-direction: column;
 
 `
+
+export const UserInfo = styled.p<ContainerProps>`
+    font-weight: 600;
+    font-size: 14px;
+    text-align: center;
+    font-family: 'Work Sans';
+    ${
+        (props) => props.variant ? css`
+            margin-top: 12px;
+        ` : css`
+            margin-top: 8px;
+        `
+    }
+`
+
 export const InfoBottom = styled.div`
     display: flex;
     flex-direction: column;
@@ -33,12 +63,21 @@ export const InfoBottom = styled.div`
     margin-top:14px;
     font-family:PT Sans;
     `
-export const InfoDescription = styled.div`
+export const InfoDescription = styled.div<ContainerProps>`
     margin-top:8px;
     font-size:12px;
     text-align:center;
     margin-bottom:3px;
     color:#757575;
+    ${
+        (props) => props.variant ? css`
+            font-family: 'PT Sans';
+            font-weight: 400;
+            line-height:  130%;
+        ` : css`
+            font-family: 'Work Sans'
+        `
+    }
 `
 
 
