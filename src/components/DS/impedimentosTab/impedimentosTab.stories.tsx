91c3233a45d77d4978cb40fb0aef103d4f927ef6
Marlon Pereira Silveira <marlonps94@gmail.com>
2024-08-22 18:16:04 -0300

    Adicionado click ao avatar e ajuste da edição do impediment tabs

--- src/components/DS/impedimentosTab/impedimentosTab.stories.tsx
+++ src/components/DS/impedimentosTab/impedimentosTab.stories.tsx
@@ -20,6 +20,7 @@ withOptions.args = {
         {
             id: '1',
             avatar: `https://xsgames.co/randomusers/avatar.php?g=male`,
+            user_name: 'Fulano teste de nome muito grande kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
             description: 'Primeiro impedimento',
             title: 'Impedimento 1',
             isGoalOwner: true,
@@ -32,6 +33,7 @@ withOptions.args = {
         {
             id: '2',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+            user_name: 'Fulano',
             description: 'Segundo impedimento',
             title: 'Impedimento 2',
             showOptions: true,
@@ -42,6 +44,7 @@ withOptions.args = {
         {
             id: '3',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Terceiro impedimento',
             title: 'Impedimento 3',
             isGoalOwner: false,
@@ -53,6 +56,7 @@ withOptions.args = {
         {
             id: '4',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Quarto impedimento',
             title: 'Impedimento 4',
             isGoalOwner: true,
@@ -64,6 +68,7 @@ withOptions.args = {
         {
             id: '5',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Quinto impedimento',
             title: 'Impedimento 5',
             isGoalOwner: true,
@@ -75,18 +80,21 @@ withOptions.args = {
         {
             id: '6',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+            user_name: 'Fulano',
             description: 'Sexto impedimento',
             title: 'Impedimento 6'
         },
         {
             id: '7',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Sétimo impedimento',
             title: 'Impedimento 7'
         },
         {
             id: '8',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Oitavo impedimento',
             title: 'Impedimento 8',
             isGoalOwner: true,
@@ -98,18 +106,21 @@ withOptions.args = {
         {
             id: '9',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Nono impedimento',
             title: 'Impedimento 9'
         },
         {
             id: '10',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Décimo impedimento',
             title: 'Impedimento 10'
         },
     ],
     onSaveNewImpedimento: (text) => console.log("Salvando impedimento: ", text),
     onSelectedTab: (impedimento) => console.log("Impedimento selecionado: ", impedimento),
+    handleClickAvatar: () => console.log("Go to profile!")
 }
 
 export const withoutOptions = Template.bind({})
@@ -121,6 +132,7 @@ withoutOptions.args = {
         {
             id: '1',
             avatar: `https://xsgames.co/randomusers/avatar.php?g=male`,
+            user_name: 'Fulano',
             description: 'Primeiro impedimento',
             title: 'Impedimento 1',
             isGoalOwner: true
@@ -128,12 +140,14 @@ withoutOptions.args = {
         {
             id: '2',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+            user_name: 'Fulano',
             description: 'Segundo impedimento',
             title: 'Impedimento 2'
         },
         {
             id: '3',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Terceiro impedimento',
             title: 'Impedimento 3',
             isGoalOwner: false
@@ -141,6 +155,7 @@ withoutOptions.args = {
         {
             id: '4',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Quarto impedimento',
             title: 'Impedimento 4',
             isGoalOwner: true
@@ -148,6 +163,7 @@ withoutOptions.args = {
         {
             id: '5',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Quinto impedimento',
             title: 'Impedimento 5',
             isGoalOwner: true
@@ -155,18 +171,21 @@ withoutOptions.args = {
         {
             id: '6',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+            user_name: 'Fulano',
             description: 'Sexto impedimento',
             title: 'Impedimento 6'
         },
         {
             id: '7',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Sétimo impedimento',
             title: 'Impedimento 7'
         },
         {
             id: '8',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Oitavo impedimento',
             title: 'Impedimento 8',
             isGoalOwner: true
@@ -174,16 +193,19 @@ withoutOptions.args = {
         {
             id: '9',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=female',
+            user_name: 'Fulano',
             description: 'Nono impedimento',
             title: 'Impedimento 9'
         },
         {
             id: '10',
             avatar: 'https://xsgames.co/randomusers/avatar.php?g=male',
+            user_name: 'Fulano',
             description: 'Décimo impedimento',
             title: 'Impedimento 10'
         },
     ],
     onSaveNewImpedimento: (text) => console.log("Salvando impedimento: ", text),
-    onSelectedTab: (impedimento) => console.log("Impedimento selecionado: ", impedimento)
+    onSelectedTab: (impedimento) => console.log("Impedimento selecionado: ", impedimento),
+    handleClickAvatar: () => console.log("Go to profile!")
 }
