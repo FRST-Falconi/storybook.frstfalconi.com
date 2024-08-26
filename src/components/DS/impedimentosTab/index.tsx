fd1aba85a2e0c4013eab71d99a5d78657c85f2ca
Marlon Pereira Silveira <marlonps94@gmail.com>
2024-08-26 16:17:04 -0300

    console log to debug

--- src/components/DS/impedimentosTab/index.tsx
+++ src/components/DS/impedimentosTab/index.tsx
@@ -49,6 +49,7 @@ export default function ImpedimentosTab({
 
     useEffect(() => {
         if(tabsList.length !== allTabs.length) {
+            console.log("Tabs list: ", tabsList)
             setAllTabs(tabsList)
         }
     }, [tabsList]);
@@ -65,6 +66,7 @@ export default function ImpedimentosTab({
     useEffect(() => {
         if (allTabs.length > 0) {
             const tabToSelect = allTabs.find(tab => tab.id === currentTab);
+            console.log("Selected tab: ", tabToSelect)
             setSelectedTab(tabToSelect ? tabToSelect : allTabs[0]);
             setOnShowTabs(allTabs.slice(0, maxTabs));
             setOnHideTabs(allTabs.slice(maxTabs, allTabs.length));
