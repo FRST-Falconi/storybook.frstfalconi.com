6a303b6ecb3d6dc9ffd815ee01985ec6ebf19af5
raquelizekk <raquelreclizek@gmail.com>
2024-09-19 16:48:41 -0300

    Alterando versão

--- src/components/users-challenge/index.tsx
+++ src/components/users-challenge/index.tsx
@@ -1,7 +1,5 @@
-import { AvatarGroup, Box, Typography, Avatar } from '@mui/material'
-import { useState } from 'react'
+import { Box } from '@mui/material'
 import * as S from './styles'
-import ModalShowUsers from '@components/modal/modalShowUsers'
 import { UsersChallengeProps } from './types'
 import UsersChallengeFeed from './variant-feed'
 import UsersChallengeForm from './variant-form'
@@ -20,56 +18,52 @@ export default function UsersChallenge({
     createData,
     variant
 }: UsersChallengeProps) {
-
     return (
         <S.Container>
-                <Box>
-                    {variant === 'feed' && (
-                        <UsersChallengeFeed
-                            areaName={areaName}
-                            avatar={avatar}
-                            companyName={companyName}
-                            createData={createData}
-                            goalUsers={goalUsers}
-                            hasOnlyAutor={hasOnlyAutor}
-                            name={name}
-                            onClickAvatar={onClickAvatar}
-                            onUserNameClick={onUserNameClick}
-                            userId={userId}
-                        />
-                    )}
-                    {variant === 'form' && (
-                        <UsersChallengeForm
-                            areaName={areaName}
-                            avatar={avatar}
-                            companyName={companyName}
-                            createData={createData}
-                            goalUsers={goalUsers}
-                            hasOnlyAutor={hasOnlyAutor}
-                            name={name}
-                            onClickAvatar={onClickAvatar}
-                            onUserNameClick={onUserNameClick}
-                            userId={userId}
-                        />
-                    )}
-                    {variant === 'vitrine' && (
-                        <UsersChallengeVitrine
-                            areaName={areaName}
-                            avatar={avatar}
-                            companyName={companyName}
-                            createData={createData}
-                            goalUsers={goalUsers}
-                            hasOnlyAutor={hasOnlyAutor}
-                            name={name}
-                            onClickAvatar={onClickAvatar}
-                            onUserNameClick={onUserNameClick}
-                            userId={userId}
-                        />
-                    )}
-                </Box>
-             
-
-          
+            <Box>
+                {variant === 'feed' && (
+                    <UsersChallengeFeed
+                        areaName={areaName}
+                        avatar={avatar}
+                        companyName={companyName}
+                        createData={createData}
+                        goalUsers={goalUsers}
+                        hasOnlyAutor={hasOnlyAutor}
+                        name={name}
+                        onClickAvatar={onClickAvatar}
+                        onUserNameClick={onUserNameClick}
+                        userId={userId}
+                    />
+                )}
+                {variant === 'form' && (
+                    <UsersChallengeForm
+                        areaName={areaName}
+                        avatar={avatar}
+                        companyName={companyName}
+                        createData={createData}
+                        goalUsers={goalUsers}
+                        hasOnlyAutor={hasOnlyAutor}
+                        name={name}
+                        onClickAvatar={onClickAvatar}
+                        onUserNameClick={onUserNameClick}
+                        userId={userId}
+                    />
+                )}
+                {variant === 'vitrine' && (
+                    <UsersChallengeVitrine
+                        areaName={areaName}
+                        avatar={avatar}
+                        companyName={companyName}
+                        createData={createData}
+                        goalUsers={goalUsers}
+                        hasOnlyAutor={hasOnlyAutor}
+                        name={name}
+                        onClickAvatar={onClickAvatar}
+                        onUserNameClick={onUserNameClick}
+                        userId={userId}
+                    />
+                )}
+            </Box>
         </S.Container>
     )
 }
