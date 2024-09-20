962d8e317382c726a2f65a8a1508acb62b772e65
Ítalo Costa <costa.italo.ti@gmail.com>
2024-09-20 10:56:34 -0300

    update profile tooltip table actions (#750)
    * update profile tooltip table actions
    
    * update dist

--- src/components/form-elements/table-actions/parts/CollaboratorAvatar/index.tsx
+++ src/components/form-elements/table-actions/parts/CollaboratorAvatar/index.tsx
@@ -3,45 +3,58 @@ import { ICollaboratorAvatar } from './collaborator-Avatar'
 import { WrapperCollaboratorAvatar } from './collaboratorAvatarStyle'
 import Avatar from '@components/avatar'
 
-export default function CollaboratorAvatar({ src, onPressAvatar, labelTextVisitProfile, uuid, align, isResponsible, userName }: ICollaboratorAvatar) {
-  return (
-    <WrapperCollaboratorAvatar align={align} >
-      {onPressAvatar && uuid ? (
-        <Tooltip
-          direction="bottom"
-          content={userName ? userName : (labelTextVisitProfile ? labelTextVisitProfile : 'Visitar perfil')}
-          trigger="hover"
-          width="fit-content"
-          height="32px"
-          style={{ top: '10px', textAlign: 'center', whiteSpace: 'nowrap' }}
-        >
-          <div onClick={(event) => {
-            event.stopPropagation()
-            onPressAvatar?.(uuid) 
-          }}>
-          <Avatar
-            size="32px"
-            src={src ? src : 'https://cdn-images.frstfalconi.cloud/path582.svg'}
-            isActiveClick={!!(onPressAvatar && uuid)}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              overflow: "hidden",
-              borderRadius: "50%", 
-              border: isResponsible ? "2px solid #31AAAA" : "2px solid transparent",
-            }}
-          />
-          </div>
-        </Tooltip>
-      ) : (
-        <Avatar
-          size="32px"
-          src={src ? src : 'https://cdn-images.frstfalconi.cloud/path582.svg'}
-          onClick={null}
-          isActiveClick={!!(onPressAvatar && uuid)}
-        />
-      )}
-    </WrapperCollaboratorAvatar>
-  )
+export default function CollaboratorAvatar({
+    src,
+    onPressAvatar,
+    labelTextVisitProfile,
+    uuid,
+    align,
+    isResponsible,
+    userName
+}: ICollaboratorAvatar) {
+    return (
+        <WrapperCollaboratorAvatar align={align}>
+            {onPressAvatar && uuid ? (
+                <Tooltip
+                    direction="right"
+                    content={userName ? userName : labelTextVisitProfile ? labelTextVisitProfile : 'Visitar perfil'}
+                    trigger="hover"
+                    width="fit-content"
+                    height="32px"
+                    style={{
+                        textAlign: 'center',
+                        whiteSpace: 'nowrap'
+                    }}
+                >
+                    <div
+                        onClick={(event) => {
+                            event.stopPropagation()
+                            onPressAvatar?.(uuid)
+                        }}
+                    >
+                        <Avatar
+                            size="32px"
+                            src={src ? src : 'https://cdn-images.frstfalconi.cloud/path582.svg'}
+                            isActiveClick={!!(onPressAvatar && uuid)}
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                overflow: 'hidden',
+                                borderRadius: '50%',
+                                border: isResponsible ? '2px solid #31AAAA' : '2px solid transparent'
+                            }}
+                        />
+                    </div>
+                </Tooltip>
+            ) : (
+                <Avatar
+                    size="32px"
+                    src={src ? src : 'https://cdn-images.frstfalconi.cloud/path582.svg'}
+                    onClick={null}
+                    isActiveClick={!!(onPressAvatar && uuid)}
+                />
+            )}
+        </WrapperCollaboratorAvatar>
+    )
 }
