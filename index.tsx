1e90b49eba6261116535d4c96a3513d0087ed7e8
Josiane Santos <josianesantos@frstfalconi.com>
2024-10-14 10:25:49 -0300

    feat: importing component

--- index.tsx
+++ index.tsx
@@ -98,6 +98,7 @@ export { default as CardUser } from './src/components/DS/cardUser'
 export { default as ImpedimentosTab } from './src/components/DS/impedimentosTab'
 export { HypothesisAndImpediment } from './src/components/DS/hypotesisAndImpediment'
 export {ProgressGoalBar} from './src/components/DS/progressGoalBar'
+export {ResultFilterTabs} from './src/components/DS/resultFilterTabs'
 export {ConfirmationModal} from './src/components/DS/ConfirmationModal'
 
 export { default as SearchField } from './src/components/search-field'
