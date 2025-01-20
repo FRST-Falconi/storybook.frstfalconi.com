19f6a35f794a65f4e7bda35d814c1eed6207a0a1
Josiane Santos <josianesantos@frstfalconi.com>
2025-01-20 11:55:54 -0300

    feat: add props em progressBar

--- src/components/LXP/progressBar/progressStyle.ts
+++ src/components/LXP/progressBar/progressStyle.ts
@@ -10,19 +10,22 @@ export const ProgressContainer = styled.div`
   }
 `
 
-export const ValueAndProgress = styled.div`
+export const ValueAndProgress = styled.div<{ colorText?: string}>`
   display: flex;
   flex-direction: row;
   align-items: center;
+  font-family: 'PT Sans';
+  color: ${({ colorText }) => colorText || '#ffffff'};
+  font-weight: 400;
 `
-
-export const BorderLinearProgress = styled(LinearProgress)`
+export const BorderLinearProgress = styled(LinearProgress)<{ bgColor?: string, trackColor?: string }>`
   width: 100%;
   height: 8px !important;
   border-radius: 8px;
-  background: rgba(255, 77, 13, 0.3) !important;
+  background: ${({ trackColor }) => trackColor || 'rgba(255, 77, 13, 0.3)'} !important;
+
 
   .MuiLinearProgress-bar {
-    background: #ff4d0d !important;
+    background: ${({ bgColor }) => bgColor || '#ff4d0d'} !important;
   }
-`
+`;
