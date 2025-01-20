f939f885549e705ce389d6cf83fa1245c796aa58
Josiane Santos <josianesantos@frstfalconi.com>
2025-01-20 16:33:43 -0300

    build

--- dist/index.js
+++ dist/index.js
@@ -132,6 +132,9 @@ var pt__namespace = /*#__PURE__*/_interopNamespace(pt);
 var ru__namespace = /*#__PURE__*/_interopNamespace(ru);
 var zh__namespace = /*#__PURE__*/_interopNamespace(zh);
 
+function Check_circle({ fill, width, height }) {
+    return (jsxRuntime.jsx("svg", { width: width ? width : '16', height: height ? height : '16', viewBox: "0 0 16 16", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: jsxRuntime.jsx("path", { d: "M7.99967 1.33333C4.32634 1.33333 1.33301 4.32666 1.33301 8C1.33301 11.6733 4.32634 14.6667 7.99967 14.6667C11.673 14.6667 14.6663 11.6733 14.6663 8C14.6663 4.32666 11.673 1.33333 7.99967 1.33333ZM11.1397 7.13999L7.80634 10.4733C7.673 10.6 7.50634 10.6667 7.33301 10.6667C7.15967 10.6667 6.993 10.6 6.85967 10.4733L4.85967 8.47333C4.59967 8.21333 4.59967 7.78666 4.85967 7.52666C5.11967 7.26666 5.54634 7.26666 5.80634 7.52666L7.33301 9.05999L10.193 6.19333C10.453 5.93333 10.8797 5.93333 11.1397 6.19333C11.3997 6.45333 11.3997 6.87999 11.1397 7.13999Z", fill: fill ?? '#1BA853' }) }));
+}
 function EditIcon({ fill, width, height }) {
     return (jsxRuntime.jsx("svg", { width: width ? width : '20', height: height ? height : '20', viewBox: "0 0 20 20", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: jsxRuntime.jsx("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M16.1788 1.92286C16.3858 1.71638 16.667 1.6 16.9607 1.6C17.2544 1.6 17.5356 1.71638 17.7426 1.92286C17.9496 2.12928 18.0654 2.4088 18.0654 2.69981C18.0654 2.99083 17.9496 3.27034 17.7426 3.47676L9.5855 11.6127C9.4439 11.7539 9.26546 11.8543 9.07141 11.9027C8.26586 12.1036 7.52969 11.3753 7.73154 10.57C7.78007 10.3765 7.88036 10.1997 8.02166 10.0588L16.1788 1.92286ZM16.9607 0C16.244 0 15.5563 0.283939 15.0489 0.790021L6.5187 9.29803C6.41591 9.40055 6.34296 9.52912 6.30766 9.66994L5.40975 13.2523C5.34143 13.5248 5.42113 13.8133 5.61971 14.0121C5.81829 14.2109 6.10663 14.291 6.3793 14.223L9.97094 13.3274C10.1114 13.2924 10.2398 13.2199 10.3423 13.1176L18.8725 4.6096C19.38 4.10345 19.6654 3.41652 19.6654 2.69981C19.6654 1.9831 19.38 1.29617 18.8725 0.790021C18.3651 0.283939 17.6774 0 16.9607 0ZM2.59582 1.89842C1.90804 1.89842 1.24801 2.17091 0.761035 2.65662C0.273994 3.1424 0 3.80169 0 4.48958V17.0277C0 17.7156 0.273995 18.3749 0.761035 18.8607C1.24801 19.3464 1.90804 19.6188 2.59582 19.6188H15.1666C15.8544 19.6188 16.5144 19.3464 17.0014 18.8607C17.4884 18.3749 17.7624 17.7156 17.7624 17.0277V10.7586C17.7624 10.3168 17.4042 9.95864 16.9624 9.95864C16.5206 9.95864 16.1624 10.3168 16.1624 10.7586V17.0277C16.1624 17.2899 16.058 17.5418 15.8715 17.7278C15.6849 17.9139 15.4314 18.0189 15.1666 18.0189H2.59582C2.33104 18.0189 2.07753 17.9139 1.89093 17.7278C1.70441 17.5418 1.6 17.2899 1.6 17.0277V4.48958C1.6 4.22738 1.70441 3.9755 1.89093 3.78946C2.07753 3.60335 2.33104 3.49842 2.59582 3.49842H8.8812C9.32303 3.49842 9.6812 3.14025 9.6812 2.69842C9.6812 2.25659 9.32303 1.89842 8.8812 1.89842H2.59582Z", fill: fill ?? '#0645AD', strokeWidth: "1.2" }) }));
 }
@@ -12644,20 +12647,24 @@ const ValueAndProgress = styled__default["default"].div `
   display: flex;
   flex-direction: row;
   align-items: center;
+  font-family: 'PT Sans';
+  color: ${({ colorText }) => colorText || '#ffffff'};
+  font-weight: 400;
 `;
 const BorderLinearProgress = styled__default["default"](LinearProgress__default["default"]) `
   width: 100%;
   height: 8px !important;
   border-radius: 8px;
-  background: rgba(255, 77, 13, 0.3) !important;
+  background: ${({ trackColor }) => trackColor || 'rgba(255, 77, 13, 0.3)'} !important;
+
 
   .MuiLinearProgress-bar {
-    background: #ff4d0d !important;
+    background: ${({ bgColor }) => bgColor || '#ff4d0d'} !important;
   }
 `;
 
-function ProgressBar$1({ value, label, style }) {
-    return (jsxRuntime.jsx(styled.ThemeProvider, { theme: FRSTTheme, children: jsxRuntime.jsxs(ProgressContainer, { style: style, children: [jsxRuntime.jsx("span", { children: label }), jsxRuntime.jsxs(ValueAndProgress, { children: [jsxRuntime.jsx(BorderLinearProgress, { variant: "determinate", value: value }), " \u00A0", value + '%'] })] }) }));
+function ProgressBar$1({ value, label, style, bgColor, trackColor, colorText }) {
+    return (jsxRuntime.jsx(styled.ThemeProvider, { theme: FRSTTheme, children: jsxRuntime.jsxs(ProgressContainer, { style: style, children: [jsxRuntime.jsx("span", { children: label }), jsxRuntime.jsxs(ValueAndProgress, { colorText: colorText, children: [jsxRuntime.jsx(BorderLinearProgress, { trackColor: trackColor, bgColor: bgColor, variant: "determinate", value: value }), " \u00A0", value + '%'] })] }) }));
 }
 
 const Container$e = styled__default["default"].div `
@@ -16518,12 +16525,16 @@ const StylesAccordionList = styled__default["default"].div `
   display: flex;
   align-items: flex-start;
   flex-direction: column;
+  border: ${({ isOldAccordionList }) => !isOldAccordionList && `1px solid #CFD9E6`};
+  border-radius: ${({ isOldAccordionList }) => !isOldAccordionList && `8px 8px 0px 0px`};  
   &.open {
     border-radius: 8px 8px 0px 0px;
-    border: ${({ theme, selectedItem }) => selectedItem && `1px solid ${theme.colors.primary1}`};
+    border: ${({ theme, selectedItem, isOldAccordionList }) => selectedItem && isOldAccordionList && `1px solid ${theme.colors.primary1}`};
   }
 
   .title {
+    font-family: 'Work Sans';
+    font-weight: ${({ selectedItem, isOldAccordionList }) => (selectedItem && !isOldAccordionList ? '600' : 'normal')};
     ::selection {
       color: inherit;
       background: transparent;
@@ -16533,21 +16544,42 @@ const StylesAccordionList = styled__default["default"].div `
   .header {
     width: 100%;
     display: flex;
-    align-items: center;
+    align-items: flex-start; /* Modificado para alinhar verticalmente */
     justify-content: space-between;
     padding: 19px 24px;
     background-color: ${({ theme }) => theme.colors.shadeWhite};
+    cursor: pointer;
+    position: relative; /* Permite a seta ficar fixada à direita */
+    
     &.open {
       border-radius: 8px 8px 0px 0px;
     }
+
     &.closed {
-      border-radius: 8px;
+      border-radius: ${({ isOldAccordionList }) => !isOldAccordionList ? '8px 8px 0px 0px;' : '8px'};
     }
-    cursor: pointer;
+
+    &:hover {
+      background: ${({ isOldAccordionList }) => !isOldAccordionList && `#EDF3F9`};
+    }
+
+    /* Coloca o título e progress bar em uma coluna */
+    .title-container {
+      display: flex;
+      flex-direction: column; /* Empilha o título e progress bar */
+      align-items: flex-start;
+    }
+
     > span {
+      position: absolute; /* Posiciona a seta à direita */
+      top: 50%; /* Alinha verticalmente */
+      right: 24px; /* Ajusta a distância à direita */
+      transform: translateY(-50%); /* Ajuste fino no alinhamento vertical */
+      
       &.open {
         animation: 0.2s open both;
       }
+
       &.closed {
         animation: 0.2s closed both;
       }
@@ -16571,8 +16603,20 @@ const StylesAccordionList = styled__default["default"].div `
     height: auto;
   }
 `;
+const ContainerComplete = styled__default["default"].div `
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+  gap: 8px;
+`;
+const CompleteText = styled__default["default"].p `
+  font-family: 'PT Sans';
+  font-size: 12px;
+  font-weight: 700;
+  color: #444444;
+`;
 
-function AccordionList$2({ title = 'Módulo', children, isOpen = false, setIsOpen, selectedItem }) {
+function AccordionList$2({ title = 'Módulo', children, isOpen = false, setIsOpen, isOldAccordionList = true, selectedItem, style = {}, value }) {
     const [open, setOpen] = React.useState(isOpen);
     const [selected, setSelected] = React.useState(selectedItem);
     const handleSetOpen = () => {
@@ -16585,7 +16629,8 @@ function AccordionList$2({ title = 'Módulo', children, isOpen = false, setIsOpe
         }
         setSelected(selectedItem);
     }, [selectedItem]);
-    return (jsxRuntime.jsxs(StylesAccordionList, { theme: FRSTTheme, selectedItem: selected, className: selected ? 'open' : 'closed', children: [jsxRuntime.jsxs("div", { onClick: handleSetOpen, className: selected ? 'header open' : 'header closed', children: [jsxRuntime.jsx("p", { className: "title", children: title }), jsxRuntime.jsx("span", { className: open ? 'open' : 'closed', children: jsxRuntime.jsx(ArrowScrollLeft, { height: "16", width: "16" }) })] }), open && jsxRuntime.jsx("div", { className: "listContent", children: children })] }));
+    return (jsxRuntime.jsxs(StylesAccordionList, { theme: FRSTTheme, selectedItem: selected, style: { ...style }, isOldAccordionList: isOldAccordionList, className: selected ? 'open' : 'closed', children: [jsxRuntime.jsxs("div", { onClick: handleSetOpen, className: selected ? 'header open' : 'header closed', children: [jsxRuntime.jsxs("div", { className: "title-container", children: [jsxRuntime.jsx("p", { className: "title", children: title }), !isOldAccordionList && value === 100 ? (jsxRuntime.jsxs(ContainerComplete, { children: [jsxRuntime.jsx(Check_circle, {}), " ", jsxRuntime.jsx(CompleteText, { children: "Completo" })] })) : (!isOldAccordionList &&
+                                jsxRuntime.jsx(material.Box, { width: '135px', marginTop: '22px', children: jsxRuntime.jsx(ProgressBar$1, { bgColor: '#F26818', value: value, trackColor: '#E0E0E0', colorText: '#444444' }) }))] }), jsxRuntime.jsx("span", { className: open ? 'open' : 'closed', children: jsxRuntime.jsx(ArrowScrollLeft, { height: "16", width: "16" }) })] }), open && jsxRuntime.jsx("div", { className: "listContent", children: children })] }));
 }
 
 const ModalNewFeaturesContainer = styled__default["default"].div `
